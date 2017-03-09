9c6cce58372d8b16aace560bcf4067371cff2c95
giblet-goblin <dgibbns@gmail.com>
2017-03-08 19:28:41 -0500

    Added time to the deployment log

--- git_deploy.php
+++ git_deploy.php
@@ -24,12 +24,18 @@
 	$remote_addr = $_SERVER['REMOTE_ADDR'];
 	$user_agent =  $_SERVER['HTTP_USER_AGENT'];
 
+	$timestamp = date("Y-m-d h:i:s", time());
+
 	// Log user info
-	$user_log = "Remote IP: " . $remote_addr . "\r\nUA: " . $user_agent . "\r\n";
-	file_put_contents ($log_file, $data, FILE_APPEND | LOCK_EX);
+	$user_log = "\n
+	Time: " . $timestamp . "\n
+	Remote IP: " . $remote_addr . "\n
+	UA: " . $user_agent . "\n";
+
+	file_put_contents($log_file, $data, FILE_APPEND | LOCK_EX);
 
 	// Update local repository
-	$command = 'git pull > ' . $log_file . ' &';
+	$command = 'git pull >> ' . $log_file . ' &';
 	shell_exec($command);
 
 ?>
