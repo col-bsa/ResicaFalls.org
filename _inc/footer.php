0cfa1b8137c4fc567d47f9fd3e92a5f7140c0dcd
Matt Gutterman <mngutterman@github.com>
2017-03-14 15:08:51 -0400

    Fixed footer link issues

--- _inc/footer.php
+++ _inc/footer.php
@@ -3,28 +3,28 @@
 		<div class="row">
 			<div class="col-12 col-md-6 text-center text-md-left">
 				&copy;<?php echo date("Y"); ?> Resica Falls <br>
-				Made with &hearts; by <a href="unamilodge.org"> Unami Lodge, One </a>
+				Made with &hearts; by <a href="http://unamilodge.org"> Unami Lodge, One </a>
 			</div>
 			<div class="col-12 col-md-6 text-center text-md-right">
-				<a href="fb.com/resicafalls">
+				<a href="http://fb.com/resicafalls">
 					<span class="fa-stack fa-lg">
   						<i class="fa fa-circle fa-stack-2x"></i>
 						<i class="fa fa-stack-1x fa-inverse fa-facebook" aria-hidden="true"></i>
 					</span>
 				</a>
-				<a href="twitter.com/resicafalls">
+				<a href="http://twitter.com/resicafalls">
 					<span class="fa-stack fa-lg">
   						<i class="fa fa-circle fa-stack-2x"></i>
 						<i class="fa fa-stack-1x fa-inverse fa-twitter" aria-hidden="true"></i>
 					</span>
 				</a>
-				<a href="instagram.com/resicafalls">
+				<a href="http://instagram.com/resicafalls">
 					<span class="fa-stack fa-lg">
   						<i class="fa fa-circle fa-stack-2x"></i>
 						<i class="fa fa-stack-1x fa-inverse fa-instagram" aria-hidden="true"></i>
 					</span>
 				</a>
-				<a href="flickr.com/resicafalls">
+				<a href="http://flickr.com/resicafalls">
 					<span class="fa-stack fa-lg">
   						<i class="fa fa-circle fa-stack-2x"></i>
 						<i class="fa fa-stack-1x fa-inverse fa-flickr" aria-hidden="true"></i>
