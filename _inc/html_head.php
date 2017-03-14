e6f1f9cff0f7e61bfce7baea1a6e38a75e47ac76
Matt Gutterman <mngutterman@github.com>
2017-03-14 16:18:20 -0400

    Added a favicon

--- _inc/html_head.php
+++ _inc/html_head.php
@@ -3,6 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <!-- Site Wide Metadata -->
+<link rel="shortcut icon" type="image/x-icon" href="_img/favicon.ico?">
 
 <!-- CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
