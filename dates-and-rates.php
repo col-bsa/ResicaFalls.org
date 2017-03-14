7c72b4ab9b9b0b246b83a83caf1554e4a3296b7b
Matt Gutterman <mngutterman@github.com>
2017-03-14 17:45:07 -0400

    Created Comming Soon and Dates & Rates pages

---
+++ dates-and-rates.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<title>Coming Soon <?php include '_inc/var/site_name.php'; ?></title>
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col">
+					<h1>Dates &amp; Rates</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 col-md-8">
+					<table class="table">
+						<thead class="thead-inverse">
+							<tr>
+								<th>Session</th>
+								<th>Dates</th>
+								<th>Resident Camp</th>
+								<th>Troop 1</th>
+								<th>Specialty Camps</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Week 1</td>
+								<td>June 25 – July 1</td>
+								<td><a href="#">Register Now!</a></td>
+								<td><a href="#">Register Now!</a></td>
+								<td></td>									
+							</tr>
+							<tr>
+								<td>Week 2</td>
+								<td>July 2 – July 8</td>
+								<td><a href="#">Register Now!</a></td>
+								<td><a href="#">Register Now!</a></td>
+								<td></td>									
+							</tr>
+							<tr>
+								<td>Week 3</td>
+								<td>July 9 – July 15</td>
+								<td><a href="#">Register Now!</a></td>
+								<td><a href="#">Register Now!</a></td>
+								<td><a href="#">NYLT</a></td></td>									
+							</tr>
+							<tr>
+								<td>Week 4</td>
+								<td>July 16 – July 22</td>
+								<td><a href="#">Register Now!</a></td>
+								<td><a href="#">Register Now!</a></td>
+								<td></td>									
+							</tr>
+							<tr>
+								<td>Week 5</td>
+								<td>July 23 – July 29</td>
+								<td><a href="#">Register Now!</a></td>
+								<td><a href="#">Register Now!</a></td>
+								<td></td>									
+							</tr>
+							<tr>
+								<td>Week 6</td>
+								<td>July 30 – Aug 5 </td>
+								<td><a href="#">Register Now!</a></td>
+								<td><a href="#">Register Now!</a></td>
+								<td></td>									
+							</tr>
+							<tr>
+								<td>Week 7</td>
+								<td>Aug 6 – Aug 12 </td>
+								<td><a href="#">Register Now!</a></td>
+								<td><a href="#">Register Now!</a></td>
+								<td><a href="#">NYLT</a></td></td>									
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-12 col-md-4">
+					<table class="table table-bordered text-center">
+						<thead class="thead-inverse">
+							<tr>
+								<th>Pricing</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr class="table-highlight">
+								<td>Early Bird Rate: $395 per Scout</td>
+							</tr>
+							<tr>
+								<td>Regular Rate: $420 per Scout</td>
+							</tr>
+							<tr>
+								<td>Adult Rate: $190 per Adult</td>
+							</tr>
+							<tr>
+								<td>Get 1 Free Adult for every 10 Scouts!</td>
+							</tr>
+							<tr>
+								<td>Your unit must be paid in full by May 15th to receive the Early Bird rate.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
