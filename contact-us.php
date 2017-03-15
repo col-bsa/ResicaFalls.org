451e54c4aea4c8d46242c6d2683617afccb1e09a
dwgibbons <dgibbns@gmail.com>
2017-03-15 16:29:02 -0400

    Fixing some errors in javascript

--- contact-us.php
+++ contact-us.php
@@ -19,25 +19,27 @@
 					<h5>Contact Form</h5>
 					<form id="contact-form">
 						<div class="form-group row">
-							<label for="forminput_name" class="col-2 col-form-label" required>Name</label>
+							<label for="forminput_name" class="col-2 col-form-label">Name</label>
 							<div class="col-10">
-								<input class="form-control" type="text" id="forminput_name">
+								<input class="form-control" type="text" id="forminput_name" required>
 							</div>
 						</div>
 						<div class="form-group row">
-							<label for="forminput_email" class="col-2 col-form-label" required>Email</label>
+							<label for="forminput_email" class="col-2 col-form-label">Email</label>
 							<div class="col-10">
-								<input class="form-control" type="email" id="forminput_email">
+								<input class="form-control" type="email" id="forminput_email" required>
 							</div>
 						</div>
-						<div class="form-group">
-							<label for="forminput_message">Message</label>
-							<textarea class="form-control" id="forminput_message" rows="4"></textarea>
+						<div class="form-group row">
+							<label for="forminput_message" class="col-2 col-form-label">Message</label>
+							<div class="col-10">
+								<textarea class="form-control" id="forminput_message" rows="4" required></textarea>
+							</div>
 						</div>
 						<button type="submit" class="btn btn-primary" id="forminput_send">Send</button>
 					</form>
 					<div class="alert fade show" role="alert" id="alert-response">
-						<strong id="alert-headline">Holy guacamole!</strong> <span id="alert-text"></span>
+						<strong id="alert-headline"></strong> <span id="alert-text"></span>
 					</div>
 				</div>
 				<div class="col-12 col-md-4">
@@ -48,7 +50,6 @@
 						East Stroudsburg, PA 18302<br>
 						<abbr title="Phone">P:</abbr> (570) 223-8312
 					</address>
-					<h5><a href="meet-the-team">Meet the Team!</a></h5>
 					<address>
 						<strong>Cradle of Liberty Council Office</strong><br>
 						1485 Valley Forge Road<br>
@@ -56,6 +57,7 @@
 						<abbr title="Phone">P:</abbr> (610) 688-6900<br>
 						<a href="http://colbsa.org" target="_blank">colbsa.org</a>
 					</address>
+					<h5><a href="meet-the-team">Meet the Team!</a></h5>
 				</div>
 			</div>
 		</div>
@@ -76,7 +78,7 @@
 					success: function(data) {
 						// ... do something with the data...
 					}
-					fail: function(data) {
+					error: function(data) {
 						$("#alert-response").addClass("alert-danger");
 						$("#alert-headline").text("Woops!");
 						$("#alert-text").text("Sorry, but it looks like something has gone wrong. Please try again later.");
