775044e0e944f86fb236a2a1d707b0ba9c0fd7ce
Matt Gutterman <mngutterman@github.com>
2017-03-14 22:37:46 -0400

    Updated the navigation with current links

--- _inc/nav.php
+++ _inc/nav.php
@@ -6,42 +6,42 @@
 	<div class="collapse navbar-collapse justify-content-md-center" id="navbar">
 		<ul class="navbar-nav">
 			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="dropdown_FutureCampers" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Future<br class="hidden-md-down"> Campers</a>
+				<a class="nav-link dropdown-toggle" href="coming-soon" id="dropdown_FutureCampers" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Future<br class="hidden-md-down"> Campers</a>
 				<div class="dropdown-menu" aria-labelledby="dropdown_FutureCampers">
-					<a class="dropdown-item" href="#">Dates &amp Rates</a>
-					<a class="dropdown-item" href="#">Programs &amp Opportunities</a>
-					<a class="dropdown-item" href="#">Food &amp Nutrition</a>
-					<a class="dropdown-item" href="#">Taste of Camp</a>
-					<a class="dropdown-item" href="#">Camp History</a>
-					<a class="dropdown-item" href="#">Schedule a Visit</a>					
+					<a class="dropdown-item" href="dates-rates">Dates &amp Rates</a>
+					<a class="dropdown-item" href="coming-soon">Programs &amp Opportunities</a>
+					<a class="dropdown-item" href="coming-soon">Food &amp Nutrition</a>
+					<a class="dropdown-item" href="coming-soon">Taste of Camp</a>
+					<a class="dropdown-item" href="camp-history">Camp History</a>
+					<a class="dropdown-item" href="schedule-a-visit">Schedule a Visit</a>					
 				</div>
 			</li>
 			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="dropdown_CurrentCampers" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Current<br class="hidden-md-down"> Campers</a>
+				<a class="nav-link dropdown-toggle" href="coming-soon" id="dropdown_CurrentCampers" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Current<br class="hidden-md-down"> Campers</a>
 				<div class="dropdown-menu" aria-labelledby="dropdown_CurrentCampers">
-					<a class="dropdown-item" href="#">Dates &amp Rates</a>
-					<a class="dropdown-item" href="#">Upcoming Events</a>
-					<a class="dropdown-item" href="#">Frequently Asked Questions</a>
-					<a class="dropdown-item" href="#">Important Documents</a>			
+					<a class="dropdown-item" href="dates-rates">Dates &amp Rates</a>
+					<a class="dropdown-item" href="coming-soon">Upcoming Events</a>
+					<a class="dropdown-item" href="coming-soon">Frequently Asked Questions</a>
+					<a class="dropdown-item" href="coming-soon">Important Documents</a>			
 				</div>
 			</li>
 			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="dropdown_StaffAndLeadership" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Staff &amp<br class="hidden-md-down"> Leadership</a>
+				<a class="nav-link dropdown-toggle" href="coming-soon" id="dropdown_StaffAndLeadership" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Staff &amp<br class="hidden-md-down"> Leadership</a>
 				<div class="dropdown-menu" aria-labelledby="dropdown_StaffAndLeadership">
-					<a class="dropdown-item" href="#">Meet the Team</a>
-					<a class="dropdown-item" href="#">Our Staff</a>
-					<a class="dropdown-item" href="#">CIT Program</a>
-					<a class="dropdown-item" href="#">Work at Resica</a>
+					<a class="dropdown-item" href="coming-soon">Meet the Team</a>
+					<a class="dropdown-item" href="coming-soon">Our Staff</a>
+					<a class="dropdown-item" href="coming-soon">CIT Program</a>
+					<a class="dropdown-item" href="coming-soon">Work at Resica</a>
 				</div>
 			</li>
 			<li class="nav-item">
-            	<a class="nav-link" href="#">Facilities<br class="hidden-md-down"> &amp Rentals</a>
+            	<a class="nav-link" href="coming-soon">Facilities<br class="hidden-md-down"> &amp Rentals</a>
             </li>
             <li class="nav-item">
-            	<a class="nav-link" href="#">Contact<br class="hidden-md-down"> Us</a>
+            	<a class="nav-link" href="coming-soon">Contact<br class="hidden-md-down"> Us</a>
             </li>
             <li class="nav-item">
-            	<a class="nav-link" href="#">Register<br class="hidden-md-down"> For Camp</a>
+            	<a class="nav-link" href="dates-rates">Register<br class="hidden-md-down"> For Camp</a>
             </li>
 		</ul>
 	</div>
