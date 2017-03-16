40f658d8442e424fbf57364c8784e0544db98d49
dwgibbons <dgibbns@gmail.com>
2017-03-15 23:34:23 -0400

    Added some error reporting on successful posts

--- _js/contact-form.js
+++ _js/contact-form.js
@@ -18,7 +18,20 @@ function ContactUs() {
 		dataType: 'json',
 		data: $("form#contact-form").serialize(),
 		success: function(data) {
-				ContactUsAlert("alert-success", "Sent!", "Thank you! You should be hearing from a member of our team shortly.")
+				obj = $.parseJSON(data);
+				if(obj.success == true)
+				{
+					alert_class = "alert-success";
+					alert_headline = "Sent!";
+					alert_text = "Thank you! You should be hearing from a member of our team shortly.";
+				}
+				else
+				{
+					alert_class = "alert-danger";
+					alert_headline = "Uh-Oh!";
+					alert_text = obj.error;
+				}
+				ContactUsAlert(alert_class, alert_headline, alert_text)
 		},
 		error: function(data) {
 			ContactUsAlert("alert-danger", "Woops!", "Sorry, but it looks like something has gone wrong. Please try again later.")
