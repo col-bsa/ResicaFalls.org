84453e6ff42767e406df61edcf5525bc6ba002dc
dwgibbons <dgibbns@gmail.com>
2017-03-15 23:47:00 -0400

    Fixing the variables that are read from post

--- _inc/ContactUs_Engine.php
+++ _inc/ContactUs_Engine.php
@@ -25,10 +25,10 @@ require_once 'reCAPTCHA_Validator.php';
  *    COLLECT HTML FORM POST DATA    *
  * * * * * * * * * * * * * * * * * * */
 
-$Name = $_POST['name'];
-$Email = $_POST['email'];
-$Message = $_POST['message'];
-$reCAPTCHA_FormResponse   = $_POST['g-recaptcha-response'];
+$Name = $_POST['forminput_name'];
+$Email = $_POST['forminput_email'];
+$Message = $_POST['forminput_message'];
+$reCAPTCHA_FormResponse = $_POST['g-recaptcha-response'];
 
 $RemoteIP = $_SERVER['REMOTE_ADDR'];
 	
