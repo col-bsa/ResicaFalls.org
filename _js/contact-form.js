8ecffb0d8ca6afa73a2a486f7de6eb0aea4b48b3
dwgibbons <dgibbns@gmail.com>
2017-03-19 00:25:32 -0400

    Added back what I thought was redundant HTML

--- _js/contact-form.js
+++ _js/contact-form.js
@@ -28,10 +28,7 @@ $("#contact-form").validate({
 $("#forminput_send").click(function() {
 	event.preventDefault();
 	if($("#contact-form").valid())
-	{
-		alert("Validity: " + $("#contact-form").valid());
 		grecaptcha.execute();
-	}
 });
 function ContactUs() {
 	var alert_class;
