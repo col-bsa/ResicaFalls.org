dff193ceb2da6504bb868c4af9425c52a18f8f8a
dwgibbons <dgibbns@gmail.com>
2017-03-19 01:54:42 -0400

    Cleaned up error messages and simple code.

--- _js/contact-form.js
+++ _js/contact-form.js
@@ -12,27 +12,33 @@ $("#contact-form").validate({
 	debug: true,
 	rules: {
 		name: {
-			required: true,
-			minlength: 2
+			required: true
 		},
 		email: {
 			required: true,
 			email: true
 		},
 		message: {
-			required: true,
-			minlength: 2
+			required: true
 		}
+	},
+	messages: {
+		name: "Please tell us your name.",
+		email: {
+			required: "Please give us your email address.",
+			email: "Please put your email address in the format of name@domain.com"
+		}
+		message: "Please let us know what you want to say.",
 	},
 	submitHandler: function() {
 		grecaptcha.execute();
 	}
 });
 function ContactUs() {
-	var alert_class;
-	var alert_headline;
-	var alert_text;
-	event.preventDefault();
+	var alert_class = "alert-info";
+	var alert_headline = "Loading...";
+	var alert_text = "";
+	ContactUsAlert(alert_class, alert_headline, alert_text)
 	$("#forminput_name").prop('disabled', true);
 	$("#forminput_email").prop('disabled', true);
 	$("#forminput_message").prop('disabled', true);
@@ -55,10 +61,12 @@ function ContactUs() {
 					alert_headline = "Uh-Oh!";
 					alert_text = data['error'];
 				}
-				ContactUsAlert(alert_class, alert_headline, alert_text)
 		},
 		error: function(data) {
-			ContactUsAlert("alert-danger", "Woops!", "Sorry, but it looks like something has gone wrong. Please try again later.")
+			alert_class = "alert-danger";
+			alert_headline = "Woops!";
+			alert_text = "Sorry, but it looks like something has gone wrong. Please try again later.";
 		}
 	});
+	ContactUsAlert(alert_class, alert_headline, alert_text);
 };
