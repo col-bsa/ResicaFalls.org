8669c9b84a4a5981c0ca892b62408b737f8c7709
Matt Gutterman <mngutterman@github.com>
2017-03-24 21:50:18 -0400

    Added placeholders to the programs page

--- programs-and-opportunities.php
+++ programs-and-opportunities.php
@@ -23,14 +23,179 @@
 				<section>
 					<div class="col">
 						<div class="card">
-							<div class="row ">
+							<div class="row">
 								<div class="col-md-4">
 									<img src="https://placehold.it/300x300" class="w-100">
 								</div>
 								<div class="col-md-8 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">Dan Beard</h4>
-										<p class="card-text">Text</p>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
+										<a href="#" class="btn btn-primary">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="card">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="https://placehold.it/300x300" class="w-100">
+								</div>
+								<div class="col-md-8 px-3">
+									<div class="card-block px-3">
+										<h4 class="card-title">ATV Program</h4>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
+										<a href="#" class="btn btn-primary">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="card">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="https://placehold.it/300x300" class="w-100">
+								</div>
+								<div class="col-md-8 px-3">
+									<div class="card-block px-3">
+										<h4 class="card-title">High 5</h4>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
+										<a href="#" class="btn btn-primary">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="card">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="https://placehold.it/300x300" class="w-100">
+								</div>
+								<div class="col-md-8 px-3">
+									<div class="card-block px-3">
+										<h4 class="card-title">Aquatics</h4>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
+										<a href="#" class="btn btn-primary">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="card">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="https://placehold.it/300x300" class="w-100">
+								</div>
+								<div class="col-md-8 px-3">
+									<div class="card-block px-3">
+										<h4 class="card-title">Handicraft</h4>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
+										<a href="#" class="btn btn-primary">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="card">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="https://placehold.it/300x300" class="w-100">
+								</div>
+								<div class="col-md-8 px-3">
+									<div class="card-block px-3">
+										<h4 class="card-title">STEM</h4>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
+										<a href="#" class="btn btn-primary">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="card">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="https://placehold.it/300x300" class="w-100">
+								</div>
+								<div class="col-md-8 px-3">
+									<div class="card-block px-3">
+										<h4 class="card-title">Cope</h4>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
+										<a href="#" class="btn btn-primary">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="card">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="https://placehold.it/300x300" class="w-100">
+								</div>
+								<div class="col-md-8 px-3">
+									<div class="card-block px-3">
+										<h4 class="card-title">Native American Village</h4>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
+										<a href="#" class="btn btn-primary">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="card">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="https://placehold.it/300x300" class="w-100">
+								</div>
+								<div class="col-md-8 px-3">
+									<div class="card-block px-3">
+										<h4 class="card-title">Scoutcraft</h4>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
+										<a href="#" class="btn btn-primary">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="card">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="https://placehold.it/300x300" class="w-100">
+								</div>
+								<div class="col-md-8 px-3">
+									<div class="card-block px-3">
+										<h4 class="card-title">Shooting Sports</h4>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
+										<a href="#" class="btn btn-primary">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="card">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="https://placehold.it/300x300" class="w-100">
+								</div>
+								<div class="col-md-8 px-3">
+									<div class="card-block px-3">
+										<h4 class="card-title">Ecology</h4>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
+										<a href="#" class="btn btn-primary">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="card">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="https://placehold.it/300x300" class="w-100">
+								</div>
+								<div class="col-md-8 px-3">
+									<div class="card-block px-3">
+										<h4 class="card-title">Sports</h4>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
 										<a href="#" class="btn btn-primary">Read More</a>
 									</div>
 								</div>
