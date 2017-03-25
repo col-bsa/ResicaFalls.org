a3a9196e5cb8f39b79cbd0d5899105351b124510
Matt Gutterman <mngutterman@github.com>
2017-03-24 22:48:46 -0400

    Added descriptions to programs

--- programs-and-opportunities.php
+++ programs-and-opportunities.php
@@ -24,14 +24,15 @@
 					<div class="col">
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">Dan Beard</h4>
-										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
+										<p class="card-text">
+											Resica Falls is proud to continue our Dan Beard First Year Camper Program. This program focuses on teaching the skills necessary for Scout through First Class. Skill sessions focus on scout skills, first aid, citizenship, and swimming. Scouts also have the opportunity to participate in a 5 mile hike and overnighter.  The program is designed to allow scouts to focus on the skills they need and supplement the rest of the day with merit badges.
+										</p>
 									</div>
 								</div>
 							</div>
@@ -39,14 +40,15 @@
 						<br>
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">ATV Program</h4>
-										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
+										<p class="card-text">
+											New to Resica Falls in 2016 is our ATV program. Spend the week riding around the reservation on one of our state-of-the-art ATVs! Scouts must be 14 years of age or older to participate in this exciting program. Throughout the week-long session, you will learn the fundamentals of riding under the supervision of a certified instructor.
+										</p>
 									</div>
 								</div>
 							</div>
@@ -54,14 +56,20 @@
 						<br>
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">High 5</h4>
-										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
+										<p class="card-text">
+											Want to try something different in the afternoon? Join in the High Five gang of adventurers and be ready to explore the secrets that Resica Falls has to offer. Scouts aged 14 and up will be given the opportunity to go on a different adventure around Resica Falls each day! Possible excursions include:
+											<ul>
+												<li>A hike to Look Out Point followed by a swim in the Bushkill Creek</li>
+												<li>A shotgun shooting competition along with black powder shooting and action archery</li>
+												<li>A canoe trip on the Delaware River</li>
+											</ul>
+										</p>
 									</div>
 								</div>
 							</div>
@@ -69,14 +77,15 @@
 						<br>
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">Aquatics</h4>
-										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
+										<p class="card-text">
+											Aquatics at Resica is fun for everyone! The pool in camp is equipped with a three-story water slide that all scouts can enjoy. In addition to canoing, kayaking, and sailing at Lake Roger, campers have the chance to get wet on the Wibit attraction, that includes a blob and trampoline!
+										</p>
 									</div>
 								</div>
 							</div>
@@ -84,14 +93,15 @@
 						<br>
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">Handicraft</h4>
-										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
+										<p class="card-text">
+											In 2016, we brought our Handicraft Lodge closer to the heart of camp. The building is now located across from the Mohawk Campsite along the camp road. The state of the art building has ample covered seating. In addition to Art, Basketry, Fingerprinting, Leatherwork, Pulp and Paper, Textile and Wood Carving, we added Pottery and Sculpture merit badges! The Handicraft Lodge also features kilns for Pottery and Sculpture!
+										</p>
 									</div>
 								</div>
 							</div>
@@ -99,14 +109,15 @@
 						<br>
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">STEM</h4>
-										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
+										<p class="card-text">
+											In 2017, Big Springs Camp has expanded its STEM Center unlike ever before! In order to maximize our science program, the STEM Center is located in The Great Bend, by the Great Bend Shower House. Badges offered include Chemistry, Digital Technology, Electricity, Electronics, Engineering, Movie Making, Photography, Radio, Robotics and Space Exploration! Make sure you stop by the STEM Center to earn a badge, or just find out more information about the program!
+										</p>
 									</div>
 								</div>
 							</div>
@@ -114,14 +125,15 @@
 						<br>
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">Cope</h4>
-										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
+										<p class="card-text">
+											Project C.O.P.E. is a unique personal development program composed of climbing, rappelling, and problem solving in both low-course and high-course settings. C.O.P.E. is unique to the participants and tests them individually and as part of a group. 
+										</p>
 									</div>
 								</div>
 							</div>
@@ -129,14 +141,15 @@
 						<br>
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">Native American Village</h4>
-										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
+										<p class="card-text">
+											Sponsered buy Unami Lodge, our Native American Village was first constructed in 2012. Using the history of the Lenni Lenape and of the Village of Ressaca, we are excited to offer Indian Lore, Archaeology and Fly Fishing Merit Badges. This will give Scouts the opportunity to learn about the history of Native Americans, as well as the history of Ressaca and how to go on archaeology digs to find relics in other communities.
+										</p>
 									</div>
 								</div>
 							</div>
@@ -144,14 +157,13 @@
 						<br>
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">Scoutcraft</h4>
 										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
 									</div>
 								</div>
 							</div>
@@ -159,14 +171,13 @@
 						<br>
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">Shooting Sports</h4>
 										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
 									</div>
 								</div>
 							</div>
@@ -174,14 +185,13 @@
 						<br>
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">Ecology</h4>
 										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
 									</div>
 								</div>
 							</div>
@@ -189,14 +199,13 @@
 						<br>
 						<div class="card">
 							<div class="row">
-								<div class="col-md-4">
-									<img src="https://placehold.it/300x300" class="w-100">
+								<div class="col-md-3">
+									<img src="https://placehold.it/250x250" class="w-100">
 								</div>
-								<div class="col-md-8 px-3">
+								<div class="col-md-9 px-3">
 									<div class="card-block px-3">
 										<h4 class="card-title">Sports</h4>
 										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis velit tenetur provident voluptates, repellat possimus, sunt laudantium nesciunt dicta, non soluta aspernatur ipsa est numquam iusto nemo necessitatibus natus.</p>
-										<a href="#" class="btn btn-primary">Read More</a>
 									</div>
 								</div>
 							</div>
