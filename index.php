1d0f715a931f657e379660c9900a05901b9ef148
Matt Gutterman <mgutterman@unamilodge.org>
2017-03-27 17:53:40 -0400

    Prepped prod for deployment

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<?php
-
-define('DS', DIRECTORY_SEPARATOR);
-
-// load kirby
-require(__DIR__ . DS . 'kirby' . DS . 'bootstrap.php');
-
-// check for a custom site.php
-if(file_exists(__DIR__ . DS . 'site.php')) {
-  require(__DIR__ . DS . 'site.php');
-} else {
-  $kirby = kirby();
-}
-
-// render
-echo $kirby->launch();
+<?php 
+header("HTTP/1.1 301 Moved Permanently"); 
+header("Location: http://colbsa.org/resica-falls-scout-reservation/"); 
+?>
--- site/templates/feed.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo page(c::get('posts-page'))->children()->visible()->limit(10)->feed(array(
-        'title'        => $page->title(),
-				'description'	 => $page->description(),
-        'link'         => c::get('posts-page'),
-        'url'          => $page->url(),
-    ));
-?>
