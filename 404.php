ba32e1b196cffbd0a05df455aa86d99b0a6676bd
dwgibbons <dgibbns@gmail.com>
2017-03-27 23:49:28 -0400

    Added 404 page.

---
+++ 404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<title>Page Not Found (404) <?php include '_inc/var/site_name.php'; ?></title>
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="slider-404">
+		</div>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col">
+					<h1>Page Not Found (404)</h1>
+				</div>
+			</div>
+			<div class="row">
+				<p>
+					<div class="h5">Sorry,</div> but it looks like you've wandered off the trail.
+				</p>
+				<p>
+					You can either use the menu above or join us back on our <a href="/index">homepage</a>.
+				</p>
+				<p>
+					If you think we made a mistake or you still need help, please let us know by <a href="contact-us">contacting us</p>.
+				</p>
+			</div>
+		</div>
+
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
