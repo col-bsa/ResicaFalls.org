4ca6abe5d3391a333678b468a07bf5d10c577152
David Gibbons <dgibbns@gmail.com>
2017-04-23 20:53:09 -0400

    Added downloads pages.

---
+++ downloads-2016.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<title>Downloads (2016) <?php include '_inc/var/site_name.php'; ?></title>
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col">
+					<h1>Downloads (2016)</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<h2>Youth</h2>
+					<div id="accordion1" role="tablist" aria-multiselectable="true">
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download1">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse1" aria-expanded="true" aria-controls="accordion1-collapse1">
+										Week 1: June 26 - July 2
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse1" class="collapse" role="tabpanel" aria-labelledby="accordion1-download1">
+								<div class="card-block">
+									<ul>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week1/" download>Slideshow</a></li></li>
+										<li><a href="https://flic.kr/s/aHskEhfvm7" target="_blank">Flickr Album</a></li></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week1/Troop103.JPG" download>Troop 103</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week1/Troop131.JPG" download>Troop 131</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week1/Troop414.JPG" download>Troop 414</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download2">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse2" aria-expanded="false" aria-controls="accordion1-collapse2">
+										Week 2: July 3 - July 9
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse2" class="collapse" role="tabpanel" aria-labelledby="accordion1-download2">
+								<div class="card-block">
+									<ul>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week2/ResicaFalls2016_Slideshow.mp4" download>Slideshow</a></li></li>
+										<li><a href="https://flic.kr/s/aHskA3odeD" target="_blank">Flickr Album</a></li></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week2/Troop18.JPG" download>Troop 18</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download3">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse3" aria-expanded="false" aria-controls="accordion1-collapse3">
+										Week 3: July 10 - July 16
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse3" class="collapse" role="tabpanel" aria-labelledby="accordion1-download3">
+								<div class="card-block">
+									<ul>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week3/ResicaFalls2016_Slideshow.mp4" download>Slideshow</a></li></li>
+										<li><a href="https://www.flickr.com/gp/resicafalls/jmDtJz" target="_blank">Flickr Album</a></li></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week3/Troop20.zip" download>Troop 20</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week3/Troop37.zip" download>Troop 37</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week3/Troop129.zip" download>Troop 129</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week3/Troop246.zip" download>Troop 246</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week3/Troop363.zip" download>Troop 363</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download4">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse4" aria-expanded="false" aria-controls="accordion1-collapse4">
+										Week 4: July 17 - July 23
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse4" class="collapse" role="tabpanel" aria-labelledby="accordion1-download4">
+								<div class="card-block">
+									<ul>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week4/ResicaFalls2016_Slideshow.mp4" download>Slideshow</a></li></li>
+										<li><a href="https://flic.kr/s/aHskE3je2W" target="_blank">Flickr Album</a></li></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week4/Troop1.zip" download>Troop 1</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week4/Troop6.zip" download>Troop 6</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week4/Troop129.zip" download>Troop 129</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week4/Troop133.zip" download>Troop 133</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week4/Troop303.zip" download>Troop 303</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week4/Troop483.zip" download>Troop 483</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download5">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse5" aria-expanded="false" aria-controls="accordion1-collapse5">
+										Week 5: July 24 - July 30
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse5" class="collapse" role="tabpanel" aria-labelledby="accordion1-download5">
+								<div class="card-block">
+									<ul>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week5/ResicaFalls2016_Slideshow.mp4" download>Slideshow</a></li></li>
+										<li><a href="https://flic.kr/s/aHskFyA5jj" target="_blank">Flickr Album</a></li></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week5/Troop98.zip" download>Troop 98</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week5/Troop371.JPG" download>Troop 371</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week5/Troop555.zip" download>Troop 555</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week5/Troop970.zip" download>Troop 970</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week5/Troop7083.zip" download>Troop 7083</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download6">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse6" aria-expanded="false" aria-controls="accordion1-collapse6">
+										Week 6: July 31 - August 6
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse6" class="collapse" role="tabpanel" aria-labelledby="accordion1-download6">
+								<div class="card-block">
+									<ul>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week6/ResicaFalls2016_Slideshow.mp4" download>Slideshow</a></li></li>
+										<li><a href="https://flic.kr/s/aHskBN6T4K" target="_blank">Flickr Album</a></li></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week6/Troop99.JPG" download>Troop 99</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week6/Troop303.zip" download>Troop 303</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week6/Troop427.JPG" download>Troop 427</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week6/Troop634.JPG" download>Troop 634</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download7">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse7" aria-expanded="false" aria-controls="accordion1-collapse7">
+										Week 7: August 7 - August 13
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse7" class="collapse" role="tabpanel" aria-labelledby="accordion1-download7">
+								<div class="card-block">
+									<ul>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week7/ResicaFalls2016_Slideshow.mp4" download>Slideshow</a></li></li>
+										<li><a href="https://flic.kr/s/aHskF4SxdN" target="_blank">Flickr Album</a></li></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week7/Troop155.zip" download>Troop 155</a></li>
+										<li><a href="https://downloads.resicafalls.org/2016_Summer/Week7/Troop252.zip" download>Troop 252</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
