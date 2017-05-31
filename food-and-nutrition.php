cb07151eb462ea059238f213d14a89405be6b456
David Gibbons <dgibbns@gmail.com>
2017-05-30 21:46:41 -0400

    Adding menu

--- food-and-nutrition.php
+++ food-and-nutrition.php
@@ -20,6 +20,336 @@
 					<p>
 						Resica Falls has partnered with FLIK Independent School Dining to provide Scouts and Scouters with a well-balanced and nutritious meal plan- approved by a certified nutritionist- for the duration of their stay in camp, beginning with dinner on Sunday. Each meal includes a hot entr&eacute;e with at least one side; lunch and dinner also include a vegetable. There is a fruit and yogurt bar available at breakfast, and a salad bar at both lunch and dinner, along with daily specialty salad bars at lunch. Peanut butter and jelly with multiple types of bread are also available at both lunch and dinner. Hot and cold drinks are available at twin juice bars located on either side of the dining hall. A sample menu for this summer can be found <a download href="_files/summer/Resica_Sample_Menu.pdf">here</a>.
 					</p>
+					<h2>Menu <div class="h5">Weeks 1,3,5,7</div></h2>
+					<table class="table table-responsive">
+						<thead class="thead-inverse">
+							<tr>
+								<td></td>
+								<td>Sunday</td>
+								<td>Monday</td>
+								<td>Tuesday</td>
+								<td>Wednesday</td>
+								<td>Thursday</td>
+								<td>Friday</td>
+								<td>Saturday</td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Breakfast</td>
+								<td> <!-- Sunday -->
+									
+								</td>
+								<td> <!-- Monday -->
+									<ul>
+										<li>Pancakes</li>
+										<li>Sausage Link</li>
+									</ul>
+								</td>
+								<td> <!-- Tuesday -->
+									<ul>
+										<li>Scrambled Eggs</li>
+										<li>Sausage Patty</li>
+										<li>Hash Brown</li>
+									</ul>
+								</td>
+								<td> <!-- Wednesday -->
+									<ul>
+										<li>Waffles with warm Syrup</li>
+										<li>Pork Roll</li>
+									</ul>
+								</td>
+								<td> <!-- Thursday -->
+									<ul>
+										<li>French Toast</li>
+										<li>Bacon</li>
+									</ul>
+								</td>
+								<td> <!-- Friday -->
+									<ul>
+										<li>Scrambled Eggs</li>
+										<li>Sausage Link</li>
+									</ul>
+								</td>
+								<td> <!-- Saturday -->
+									<ul>
+										<li>Bagels <small>with</small></li>
+										<li><small>Butter</small></li>
+										<li><small>Cream Cheese</small></li>
+										<li><small>Jelly</small></li>
+										<li><small>Peanut Butter</small></li>
+										<li>Fruit Salad</li>
+										<li>Low-Fat Yogurt</li>
+										<li>Cottage Cheese</li>
+									</ul>
+								</td>
+							</tr>
+							<tr>
+								<td>Lunch</td>
+								<td> <!-- Sunday -->
+									
+								</td>
+								<td> <!-- Monday -->
+									<ul>
+										<li></li>
+									</ul>
+								</td>
+								<td> <!-- Tuesday -->
+									<ul>
+										<li></li>
+									</ul>
+								</td>
+								<td> <!-- Wednesday -->
+									<ul>
+										<li></li>
+									</ul>
+								</td>
+								<td> <!-- Thursday -->
+									<ul>
+										<li></li>
+									</ul>
+								</td>
+								<td> <!-- Friday -->
+									<ul>
+										<li></li>
+									</ul>
+								</td>
+								<td> <!-- Saturday -->
+									
+								</td>
+							</tr>
+							<tr>
+								<td>Salad</td>
+								<td></td>
+								<td>Chef's</td>
+								<td>Chopped</td>
+								<td>Caesar</td>
+								<td>Buffalo Chicken</td>
+								<td>Taco</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Dinner</td>
+								<td> <!-- Sunday -->
+									<ul>
+										<li>Roasted Turkey</li>
+										<li>Mashed Potatoes with Gravy</li>
+										<li>Stuffing</li>
+										<li>Steamed Carrots</li>
+										<li>Apple Cobbler</li>
+									</ul>
+								</td>
+								<td> <!-- Monday -->
+									<ul>
+										<li>BBQ Chicken</li>
+										<li>Mac &amp; Cheese</li>
+										<li>Summer Squash</li>
+										<li>Brownie</li>
+									</ul>
+								</td>
+								<td> <!-- Tuesday -->
+									<ul>
+										<li>Spaghetti with Meatballs</li>
+										<li>Roasted Broccoli</li>
+										<li>Breadsticks</li>
+										<li>Ice Cream Sandwich</li>
+									</ul>
+								</td>
+								<td> <!-- Wednesday -->
+									<ul>
+										<li>Meatloaf</li>
+										<li>Rice Pilaf</li>
+										<li>Steamed Green Beans</li>
+										<li>Yellow Cake with Vanilla Frosting</li>
+									</ul>
+								</td>
+								<td> <!-- Thursday -->
+									<ul>
+										<li>Chicken Parmesan</li>
+										<li>Rigatoni</li>
+										<li>Roasted Cauliflower</li>
+										<li>Ice Cream Cup</li>
+									</ul>
+								</td>
+								<td> <!-- Friday -->
+									<ul>
+										<li>Roast Beef</li>
+										<li>Herbed Potatoes</li>
+										<li>Steamed Snap Peas</li>
+										<li>Chocolate Cake with Vanilla Frosting</li>
+									</ul>
+								</td>
+								<td> <!-- Saturday -->
+									
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<h5>Weeks 2,4,6</h5>
+					<table class="table table-responsive">
+						<thead class="thead-inverse">
+							<tr>
+								<td></td>
+								<td>Sunday</td>
+								<td>Monday</td>
+								<td>Tuesday</td>
+								<td>Wednesday</td>
+								<td>Thursday</td>
+								<td>Friday</td>
+								<td>Saturday</td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Breakfast</td>
+								<td> <!-- Sunday -->
+									
+								</td>
+								<td> <!-- Monday -->
+									<ul>
+										<li>Pancakes</li>
+										<li>Sausage Link</li>
+									</ul>
+								</td>
+								<td> <!-- Tuesday -->
+									<ul>
+										<li>Scrambled Eggs</li>
+										<li>Sausage Patty</li>
+										<li>Hash Brown</li>
+									</ul>
+								</td>
+								<td> <!-- Wednesday -->
+									<ul>
+										<li>Waffles with warm Syrup</li>
+										<li>Pork Roll</li>
+									</ul>
+								</td>
+								<td> <!-- Thursday -->
+									<ul>
+										<li>French Toast</li>
+										<li>Bacon</li>
+									</ul>
+								</td>
+								<td> <!-- Friday -->
+									<ul>
+										<li>Scrambled Eggs</li>
+										<li>Sausage Link</li>
+									</ul>
+								</td>
+								<td> <!-- Saturday -->
+									<ul>
+										<li>Bagels <small>with</small></li>
+										<li><small>Butter</small></li>
+										<li><small>Cream Cheese</small></li>
+										<li><small>Jelly</small></li>
+										<li><small>Peanut Butter</small></li>
+										<li>Fruit Salad</li>
+										<li>Low-Fat Yogurt</li>
+										<li>Cottage Cheese</li>
+									</ul>
+								</td>
+							</tr>
+							<tr>
+								<td>Lunch</td>
+								<td> <!-- Sunday -->
+									
+								</td>
+								<td> <!-- Monday -->
+									<ul>
+										<li></li>
+									</ul>
+								</td>
+								<td> <!-- Tuesday -->
+									<ul>
+										<li></li>
+									</ul>
+								</td>
+								<td> <!-- Wednesday -->
+									<ul>
+										<li></li>
+									</ul>
+								</td>
+								<td> <!-- Thursday -->
+									<ul>
+										<li></li>
+									</ul>
+								</td>
+								<td> <!-- Friday -->
+									<ul>
+										<li></li>
+									</ul>
+								</td>
+								<td> <!-- Saturday -->
+									
+								</td>
+							</tr>
+							<tr>
+								<td>Salad</td>
+								<td></td>
+								<td>Chef's</td>
+								<td>Chopped</td>
+								<td>Caesar</td>
+								<td>Buffalo Chicken</td>
+								<td>Taco</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Dinner</td>
+								<td> <!-- Sunday -->
+									<ul>
+										<li>Roasted Turkey</li>
+										<li>Mashed Potatoes with Gravy</li>
+										<li>Stuffing</li>
+										<li>Steamed Carrots</li>
+										<li>Apple Cobbler</li>
+									</ul>
+								</td>
+								<td> <!-- Monday -->
+									<ul>
+										<li>BBQ Chicken</li>
+										<li>Mac &amp; Cheese</li>
+										<li>Summer Squash</li>
+										<li>Brownie</li>
+									</ul>
+								</td>
+								<td> <!-- Tuesday -->
+									<ul>
+										<li>Spaghetti with Meatballs</li>
+										<li>Roasted Broccoli</li>
+										<li>Breadsticks</li>
+										<li>Ice Cream Sandwich</li>
+									</ul>
+								</td>
+								<td> <!-- Wednesday -->
+									<ul>
+										<li>Meatloaf</li>
+										<li>Rice Pilaf</li>
+										<li>Steamed Green Beans</li>
+										<li>Yellow Cake with Vanilla Frosting</li>
+									</ul>
+								</td>
+								<td> <!-- Thursday -->
+									<ul>
+										<li>Chicken Parmesan</li>
+										<li>Rigatoni</li>
+										<li>Roasted Cauliflower</li>
+										<li>Ice Cream Cup</li>
+									</ul>
+								</td>
+								<td> <!-- Friday -->
+									<ul>
+										<li>Roast Beef</li>
+										<li>Herbed Potatoes</li>
+										<li>Steamed Snap Peas</li>
+										<li>Chocolate Cake with Vanilla Frosting</li>
+									</ul>
+								</td>
+								<td> <!-- Saturday -->
+									
+								</td>
+							</tr>
+						</tbody>
+					</table>
 					<p>
 						Campers and adult leaders with dietary restrictions can note their individual restrictions with the camp’s FLIK food service manager before they come to camp. FLIK is more than happy to accommodate any dietary restrictions to ensure that campers and leaders have the best dining experience possible. For more information, or to discuss dietary restrictions, please contact Marco Morales, FLIK regional manager, at <a href="mailto:MARCO.MORALES@COMPASS-USA.COM">marco.morales@compass-usa.com</a>.
 					</p>
