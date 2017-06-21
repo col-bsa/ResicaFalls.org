d5ae1c973a62b5d139fa9ac12b460675b83bc92a
David Gibbons <dgibbns@gmail.com>
2017-06-21 15:43:43 -0400

    Added Webelos Wednesday

---
+++ webeloswednesday.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<meta name="description" content="An opportunity for Webelos to spend a day of Boy Scout resident camping at Resica Falls Scout Reservation!" />
+		<title>Webelos Wednesday <?php include '_inc/var/site_name.php'; ?></title>
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col">
+					<h1>Webelos Wednesday</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8">
+					<p>
+						Have a taste of the Boy Scout resident camping experience by joining us on Webelos Wednesday at Resica Falls! A wide assortment of program areas have opened their doors to Cub Scouts to explore. We have:
+						<ul>
+							<li>BB Shooting</li>
+							<li>Archery</li>
+							<li>Wilderness Cooking</li>
+							<li>Native American Games</li>
+							<li>STEM Activities</li>
+							<li>Handicraft Projects</li>
+							<li>Pool &amp; Slide Activities</li>
+							<li>Closing Campfire</li>
+							<li>...and much, much more!</li>
+						</ul>
+					</p>
+					<h2>General Information</h2>
+					<ul>
+						<li>Arrive at Camp: 9:30am</li>
+						<li>Depart Camp: 5:30pm</li>
+						<li>Lunch, Patch &amp; Camp Gift included</li>
+						<li>Participants must bring Parts A &amp; B of the BSA Medical</li>
+						<li>Questions? <a href="">Contact Us</a></li>
+					</ul>
+					<h2>Prices</h2>
+					<ul>
+						<li>Cubs: $15</li>
+						<li>Adults: $10</li>
+					</ul>
+					<h2>Register Now!</h2>
+					<ul>
+						<li><a target="_blank" href="http://colbsa.doubleknot.com/event/webelos-wednesdays-at-resica-falls-july-5-2017/2126223">July 5</a></li>
+						<li><a target="_blank" href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-july-12-2017/2127628">July 12</a></li>
+						<li><a target="_blank" href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-july-19-2017/2127648">July 19</a></li>
+						<li><a target="_blank" href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-july-26-2017/2127649">July 26</a></li>
+						<li><a target="_blank" href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-august-2-2017/2127650">August 2</a></li>
+					</ul>
+					<p>
+						Make sure to bring Parts A &amp; B of the Boy Scout Medical form for this event! (These are the parts that do not require a doctor's signature.)
+					</p>
+				</div>
+				<div class="col-md-4">
+					<img class="img-fluid rounded" src="_img/food_nutrition-1.jpg" alt="">
+					<br><br>
+					<img class="img-fluid rounded" src="_img/food_nutrition-2.jpg" alt="">
+					<br><br>
+					<img class="img-fluid rounded" src="_img/food_nutrition-3.jpg" alt="">
+				</div>
+			</div>
+		</div>
+
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
