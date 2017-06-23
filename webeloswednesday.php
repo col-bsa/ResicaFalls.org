436c688773af2fbdaac56a03dbff68ed9c672116
David Gibbons <dgibbns@gmail.com>
2017-06-23 12:08:44 -0400

    Making Webelos prettier

--- webeloswednesday.php
+++ webeloswednesday.php
@@ -20,49 +20,51 @@
 					<p>
 						Have a taste of the Boy Scout resident camping experience by joining us on Webelos Wednesday at Resica Falls! A wide assortment of program areas have opened their doors to Cub Scouts to explore.
 					</p>
-					<h4>We Have</h4>
-					<ul>
-						<li>BB Shooting</li>
-						<li>Archery</li>
-						<li>Wilderness Cooking</li>
-						<li>Native American Games</li>
-						<li>STEM Activities</li>
-						<li>Handicraft Projects</li>
-						<li>Pool &amp; Slide Activities</li>
-						<li>Closing Campfire</li>
-						<li>...and much, much more!</li>
-					</ul>
-					<h4>General Information</h4>
-					<ul>
-						<li>Arrive at Camp: 9:30am</li>
-						<li>Depart Camp: 5:30pm</li>
-						<li>Lunch, Patch &amp; Camp Gift included</li>
-						<li>Participants must bring Parts A &amp; B of the BSA Medical</li>
-						<li>Questions? <a href="contact-us">Contact Us</a></li>
-					</ul>
-					<h4>Prices</h4>
-					<ul>
-						<li>Cubs: $15</li>
-						<li>Adults: $10</li>
-					</ul>
-					<h4>Register Now!</h4>
-					<ul>
-						<li>
-							<a href="http://colbsa.doubleknot.com/event/webelos-wednesdays-at-resica-falls-july-5-2017/2126223" target="_blank">July 5</a>
-						</li>
-						<li>
-							<a href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-july-12-2017/2127628" target="_blank">July 12</a>
-						</li>
-						<li>
-							<a href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-july-19-2017/2127648" target="_blank">July 19</a>
-						</li>
-						<li>
-							<a href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-july-26-2017/2127649" target="_blank">July 26</a>
-						</li>
-						<li>
-							<a href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-august-2-2017/2127650" target="_blank">August 2</a>
-						</li>
-					</ul>
+					<div class="row">
+						<div class="col-md-6 alert alert-success">
+							<h4>Programs</h>
+							<ul>
+								<li>BB Shooting</li>
+								<li>Archery</li>
+								<li>Wilderness Cooking</li>
+								<li>Native American Games</li>
+								<li>STEM Activities</li>
+								<li>Handicraft Projects</li>
+								<li>Pool &amp; Slide Activities</li>
+								<li>Closing Campfire</li>
+								<li>...and much, much more!</li>
+							</ul>
+						</div>
+						<div class="col-md-6 alert alert-info">
+							<h4>Information</h4>
+							<ul>
+								<li>Arrive at Camp: 9:30am</li>
+								<li>Depart Camp: 5:30pm</li>
+								<li>Lunch, Patch &amp; Camp Gift included</li>
+								<li>Participants must bring Parts A &amp; B of the BSA Medical</li>
+								<li>Questions? <a href="contact-us">Contact Us</a></li>
+							</ul>
+							<h4>Prices</h4>
+							<ul>
+								<li>Cubs: $15</li>
+								<li>Adults: $10</li>
+							</ul>
+						</div>
+					</div>
+\					<div class="alert alert-success">
+						<h2 class="text-center">Register Now!</h2>
+						<div class="row justify-content-md-center">
+							<div class="col-md-6">
+								<div class="list-group">
+									<a href="http://colbsa.doubleknot.com/event/webelos-wednesdays-at-resica-falls-july-5-2017/2126223" target="_blank" class="list-group-item list-group-item-action">July 5</a>
+									<a href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-july-12-2017/2127628" target="_blank" class="list-group-item list-group-item-action">July 12</a>
+									<a href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-july-19-2017/2127648" target="_blank" class="list-group-item list-group-item-action">July 19</a>
+									<a href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-july-26-2017/2127649" target="_blank" class="list-group-item list-group-item-action">July 26</a>
+									<a href="http://colbsa.doubleknot.com/event/webelos-wednesday-at-resica-falls-august-2-2017/2127650" target="_blank" class="list-group-item list-group-item-action">August 2</a>
+								</div>
+							</div>
+						</div>
+					</div>
 					<p>
 						Make sure to bring Parts A &amp; B of the Boy Scout Medical form for this event! (These are the parts that do not require a doctor's signature.)
 					</p>
