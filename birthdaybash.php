641026b992802883ab612a06952349f14042313e
Matt Gutterman <mngutterman@gmail.com>
2017-07-03 17:30:20 -0400

    Fixed the forwarder

--- birthdaybash.php
+++ birthdaybash.php
@@ -1,6 +1,6 @@
 <?php 
 
-header("Location: http://www.yourwebsite.com/user.phphttp://colbsa.doubleknot.com/registration/calendardetail.aspx?ActivityKey=2113714&OrgKey=1112"); /* Redirect browser */
+header("Location: http://colbsa.doubleknot.com/registration/calendardetail.aspx?ActivityKey=2113714&OrgKey=1112"); /* Redirect browser */
 exit();
 
 ?>
