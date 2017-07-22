e819b01abcbb6a5f552d7605d9976fef7205da08
Matt Gutterman <mngutterman@gmail.com>
2017-07-22 10:07:41 -0400

    Fixed yearbook forwarder

--- yearbook.php
+++ yearbook.php
@@ -1,6 +1,6 @@
 <?php 
 
-header("Location: https://goo.gl/form/ztdshcxe2dxuf4wk1"); /* Redirect browser */
+header("https://docs.google.com/forms/d/e/1FAIpQLSc4CDRcc2RsXvCmHZglGuGkq0WoyMZ9cgMGLxtqVQUZpnnXbg/viewform"); /* Redirect browser */
 exit();
 
 ?>
