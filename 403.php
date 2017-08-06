ae4afe7f3a2d43ebd781a89b7af223ac7cd8ce50
David Gibbons <dgibbns@gmail.com>
2017-08-06 13:08:18 -0400

    Added 403 error page

---
+++ 403.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<base href="https://resicafalls.org">
+		<?php include '_inc/html_head.php'; ?>
+		<meta name="description" content="Looks like you wandered into a staff only area at Resica Falls Scout Reservation." />
+		<title>Forbidden (403) <?php include '_inc/var/site_name.php'; ?></title>
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="slider-404">
+		</div>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col">
+					<h1>Forbidden (403)</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<p>
+						<strong>Sorry,</strong> but it looks like you wandered into a restricted area.
+					</p>
+					<p>
+						You can either use the menu above or join us back on our <a href="/index">homepage</a>.
+					</p>
+					<p>
+						If you think we made a mistake or you still need help, please let us know by <a href="/contact-us">contacting us</a>.
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
