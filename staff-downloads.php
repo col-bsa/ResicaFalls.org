176997f33e503dc5ff060c4b66d137a8b5321b05
David Gibbons <dgibbns@gmail.com>
2017-08-06 17:28:48 -0400

    Added staff downloads

---
+++ staff-downloads.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<meta name="description" content="Resica Falls Scout Reservation downloads for staff." />
+		<title>Staff Downloads <?php include '_inc/var/site_name.php'; ?></title>
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col">
+					<h1>Staff Downloads</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<ul>
+						<li><a href="https://downloads.resicafalls.org/Closing-Report-Template.doc" download>Closing Report Template</a></li>
+						<li><a href="https://downloads.resicafalls.org/Closing-Inventory-Template.xlsx" download>Closing Inventory Template</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
