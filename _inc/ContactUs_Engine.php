c4602ee9d4197dd20e6f4ac5811d1e2498bb1930
David Gibbons <dgibbns@gmail.com>
2018-02-03 17:34:30 -0500

    Testing mailgun responses

--- _inc/ContactUs_Engine.php
+++ _inc/ContactUs_Engine.php
@@ -87,7 +87,7 @@ if(!isset($error_text))
 		'text'			=> $send_text
 	));
 
-	echo var_dump($mg);
+	$error_text =  var_dump($mg);
 
 	/* * * * * * * * * * * * * * * * * * *
 	 *          DATABASE INSERT          *
