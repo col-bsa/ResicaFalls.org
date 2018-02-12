c40be1cac7e16cb4b7a13bba852615f2146459fc
David Gibbons <dgibbns@gmail.com>
2018-02-11 22:57:10 -0500

    Undoing moving from alpha

--- _inc/html_head.php
+++ _inc/html_head.php
@@ -6,7 +6,7 @@
 <link rel="shortcut icon" type="image/x-icon" href="_img/favicon.ico?">
 
 <!-- CSS -->
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="_css/site.css">
 
 <!-- Other Meta Data -->
