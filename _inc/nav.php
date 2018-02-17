92e7a30d227a8d3d07c4f710461dd76bc6e9700e
David Gibbons <dgibbns@gmail.com>
2018-02-17 02:12:27 -0500

    Adding dropdown indicator

--- _inc/nav.php
+++ _inc/nav.php
@@ -6,32 +6,41 @@
 	<div class="collapse navbar-collapse justify-content-md-center" id="navbar">
 		<ul class="navbar-nav">
 			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="coming-soon" id="dropdown_AboutUs" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About<br class="hidden-md-down"> Us</a>
-				<div class="dropdown-menu" aria-labelledby="dropdown_AboutUs">
-					<a class="dropdown-item" href="camp-history">Camp History</a>
-					<a class="dropdown-item" href="frequently-asked-questions">Frequently Asked Questions</a>
-					<a class="dropdown-item" href="important-documents">Important Documents</a>
-					<a class="dropdown-item" href="schedule-a-visit">Schedule a Visit</a>
-					<a class="dropdown-item" href="hunting-fishing">Hunting &amp; Fishing</a>
+				<div class="btn-group navbar-btn">
+					<a class="nav-link" href="coming-soon" id="dropdown_AboutUs" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About<br class="hidden-md-down"> Us</a>
+					<a class="nav-link dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"><span class="sr-only"></span></a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown_AboutUs">
+						<a class="dropdown-item" href="camp-history">Camp History</a>
+						<a class="dropdown-item" href="frequently-asked-questions">Frequently Asked Questions</a>
+						<a class="dropdown-item" href="important-documents">Important Documents</a>
+						<a class="dropdown-item" href="schedule-a-visit">Schedule a Visit</a>
+						<a class="dropdown-item" href="hunting-fishing">Hunting &amp; Fishing</a>
+					</div>
 				</div>
 			</li>
 			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="coming-soon" id="dropdown_FutureCampers" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Summer<br class="hidden-md-down"> Programs</a>
-				<div class="dropdown-menu" aria-labelledby="dropdown_FutureCampers">
-					<a class="dropdown-item" href="dates-and-rates">Dates &amp; Rates</a>
-					<a class="dropdown-item" href="programs-and-opportunities">Programs &amp; Opportunities</a>
-					<a class="dropdown-item" href="food-and-nutrition">Food &amp; Nutrition</a>
-					<a class="dropdown-item" href="important-dates">Important Dates</a>
-					<a class="dropdown-item" href="important-documents">Important Documents</a>
-					<a class="dropdown-item" href="webeloswednesday">Webelos Wednesday</a>
+				<div class="btn-group navbar-btn">
+					<a class="nav-link" href="coming-soon" id="dropdown_FutureCampers" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Summer<br class="hidden-md-down"> Programs</a>
+					<a class="nav-link dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"><span class="sr-only"></span></a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown_FutureCampers">
+						<a class="dropdown-item" href="dates-and-rates">Dates &amp; Rates</a>
+						<a class="dropdown-item" href="programs-and-opportunities">Programs &amp; Opportunities</a>
+						<a class="dropdown-item" href="food-and-nutrition">Food &amp; Nutrition</a>
+						<a class="dropdown-item" href="important-dates">Important Dates</a>
+						<a class="dropdown-item" href="important-documents">Important Documents</a>
+						<a class="dropdown-item" href="webeloswednesday">Webelos Wednesday</a>
+					</div>
 				</div>
 			</li>
 			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="coming-soon" id="dropdown_StaffAndLeadership" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Staff &amp;<br class="hidden-md-down"> Leadership</a>
-				<div class="dropdown-menu" aria-labelledby="dropdown_StaffAndLeadership">
-					<a class="dropdown-item" href="meet-the-team">Meet the Team</a>
-					<a class="dropdown-item" href="cit-program">CIT Program</a>
-					<a class="dropdown-item" href="work-at-resica">Work at Resica</a>
+				<div class="btn-group navbar-btn">
+					<a class="nav-link" href="coming-soon" id="dropdown_StaffAndLeadership" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Staff &amp;<br class="hidden-md-down"> Leadership</a>
+					<a class="nav-link dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"><span class="sr-only"></span></a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown_StaffAndLeadership">
+						<a class="dropdown-item" href="meet-the-team">Meet the Team</a>
+						<a class="dropdown-item" href="cit-program">CIT Program</a>
+						<a class="dropdown-item" href="work-at-resica">Work at Resica</a>
+					</div>
 				</div>
 			</li>
 			<li class="nav-item">
