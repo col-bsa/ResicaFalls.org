2625f73c3a94f38cdbf1825cb496989aaaa08a1b
David Gibbons <dgibbns@gmail.com>
2018-03-05 22:55:16 -0500

    Working on Weekend Camping page

---
+++ weekend-camping.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<meta name="description" content="The team at Resica Falls Scout Reservation is still working on this page. Please check back again later!" />
+		<title>Weekend Camping <?php include '_inc/var/site_name.php'; ?></title>
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col-md-8">
+					<h1>Weekend Camping</h1>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d776294.7585002417!2d-75.81342861394013!3d40.53272017543919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x89c484e0c4004cf5%3A0xa6a105ee2d4750e0!2sResica+Falls+Scout+Reservation%2C+1200+Resica+Falls+Rd%2C+East+Stroudsburg%2C+PA+18302!3m2!1d41.111207799999995!2d-75.094943!5e0!3m2!1sen!2sus!4v1520307269276" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+				<div class="col-md-4">
+					<img class="img-fluid rounded" src="_img/coming_soon.jpg" alt="">
+					<a class="btn btn-primary btn-block" href="http://colbsa.doubleknot.com/facilitycalendar/2849">Facilities &amp; Rentals</a>
+				</div>
+			</div>
+		</div>
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
