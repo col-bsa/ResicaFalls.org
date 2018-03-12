92faa1b09c65f8add3fa30193cc42961713ee104
David Gibbons <dgibbns@gmail.com>
2018-03-12 15:15:08 -0400

    Added facility to weekend page and updated closed status

--- weekend-camping.php
+++ weekend-camping.php
@@ -13,18 +13,119 @@
 			<h1>Weekend Camping</h1>
 			<div class="row">
 				<div class="col-md-8">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d776294.7585002417!2d-75.81342861394013!3d40.53272017543919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x89c484e0c4004cf5%3A0xa6a105ee2d4750e0!2sResica+Falls+Scout+Reservation%2C+1200+Resica+Falls+Rd%2C+East+Stroudsburg%2C+PA+18302!3m2!1d41.111207799999995!2d-75.094943!5e0!3m2!1sen!2sus!4v1520307269276" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-					<p><a href="_files/general/ResicaFalls_Directions.pdf" download>Download directions from I-476</a></p>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3006.067916240707!2d-75.09713168415983!3d41.11120777929046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c484e0c4004cf5%3A0xa6a105ee2d4750e0!2sResica+Falls+Scout+Reservation!5e0!3m2!1sen!2sus!4v1520542742548" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+				<a href="_files/general/ResicaFalls_Directions.pdf" download>Download directions from I-476</a>
 				</div>
 				<div class="col-md-4">
 					<div class="card">
 						<img class="card-img-top img-fluid" src="_img/generic/unami.jpg" alt="">
 						<div class="card-block">
-							<h5 class="card-title">Facilities &amp; Rentals</h5>
+							<h5 class="card-title">Book a Stay</h5>
 							<p class="card-text">Cabins, campsites and backpacking sites available year-round for weekend camping.</p>
 							<a href="http://colbsa.doubleknot.com/facilitycalendar/2849" class="btn btn-primary">Check Availability</a>
 						</div>
 					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Lodges</h2>
+					<table class="table text-center">
+						<thead class="thead-inverse">
+							<tr>
+								<th class="text-center">Name</th>
+								<th class="text-center">Capacity</th>
+								<th class="text-center">Heat</th>
+								<th class="text-center">Water</th>
+								<th class="text-center">Fireplace</th>
+								<th class="text-center">Woodstove</th>
+								<th class="text-center">Fire Ring</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Bear</td>
+								<td>24</td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+							</tr>
+							<tr>
+								<td>Brooks</td>
+								<td>14</td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+							</tr>
+							<tr>
+								<td>Bushkill</td>
+								<td>32</td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Cox</td>
+								<td>14</td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+							</tr>
+							<tr>
+								<td>Family Cabin</td>
+								<td>4</td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+							</tr>
+							<tr>
+								<td>Firestone</td>
+								<td>40</td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+							</tr>
+							<tr>
+								<td>Nuneville</td>
+								<td>20</td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+							</tr>
+							<tr>
+								<td>Trooper</td>
+								<td>20</td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+							</tr>
+							<tr>
+								<td>Unami</td>
+								<td>26</td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+								<td><i class="fas fa-square fa-lg"></td>
+								<td><i class="fas fa-check-square fa-lg"></td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
