cd594423f54893cedd69795ee4387310edfa25df
David Gibbons <dgibbns@gmail.com>
2018-03-12 16:17:51 -0400

    Made tables side-by-side on weekend campig

--- weekend-camping.php
+++ weekend-camping.php
@@ -27,377 +27,383 @@
 					</div>
 				</div>
 			</div>
-			<h2>Lodges</h2>
-			<div class="table-responsive">
-				<table class="table table-sm text-center">
-					<thead class="thead-inverse">
-						<tr>
-							<th class="text-center">Name</th>
-							<th class="text-center">Capacity</th>
-							<th class="text-center">Heat</th>
-							<th class="text-center">Water</th>
-							<th class="text-center">Fireplace</th>
-							<th class="text-center">Woodstove</th>
-							<th class="text-center">Fire Ring</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>Bear</td>
-							<td>24</td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Brooks</td>
-							<td>14</td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Bushkill</td>
-							<td>32</td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-						</tr>
-						<tr>
-							<td>Cox</td>
-							<td>14</td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Family Cabin</td>
-							<td>4</td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Firestone</td>
-							<td>40</td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Nuneville</td>
-							<td>20</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-						</tr>
-						<tr>
-							<td>Trooper</td>
-							<td>20</td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-						</tr>
-						<tr>
-							<td>Unami</td>
-							<td>26</td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-			<h2>Campsites</h2>
-			<div class="table-responsive">
-				<table class="table table-sm text-center">
-					<thead class="thead-inverse">
-						<tr>
-							<th class="text-center">Name</th>
-							<th class="text-center">Capacity</th>
-							<th class="text-center">Tent Platforms</th>
-							<th class="text-center">Adirondacks</th>
-							<th class="text-center">Pavilion</th>
-							<th class="text-center">Latrine</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>Little Falls - North</td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-						</tr>
-						<tr>
-							<td>Little Falls - South</td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-						</tr>
-						<tr>
-							<td>Sportsman Field</td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-						</tr>
-						<tr>
-							<td>Big Trout</td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-						</tr>
-						<tr>
-							<td>Blueberry Point</td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-						</tr>
-						<tr>
-							<td>Lake Roger</td>
-							<td>25</td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Pine Grove</td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-						</tr>
-						<tr>
-							<td>Bear Hollow</td>
-							<td>35</td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Bear Paw</td>
-							<td>40</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Cayuga</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Cherokee</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Cheyenne</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Chinook</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Chippewa</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Comanche</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Deer Run</td>
-							<td>40</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Eagle's Crest</td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Falcons Roost</td>
-							<td>35</td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Firestone Staff Area</td>
-							<td>35</td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Hillside</td>
-							<td>35</td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Mohawk</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Navajo</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>North Wind</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Porcupine Ridge</td>
-							<td>35</td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Pueblo</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Ruffed Grouse</td>
-							<td>35</td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Running Fox</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Shenandoah</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Shoshone</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Twin Hill 1</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Twin Hill 2</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-						<tr>
-							<td>Two Feathers</td>
-							<td>35</td>
-							<td><i class="fa fa-check"></td>
-							<td></td>
-							<td></td>
-							<td><i class="fa fa-check"></td>
-						</tr>
-					</tbody>
-				</table>
+			<div class="row">
+				<div class="col-md-6">
+					<h2>Lodges</h2>
+					<div class="table-responsive">
+						<table class="table table-sm text-center">
+							<thead class="thead-inverse">
+								<tr>
+									<th class="text-center">Name</th>
+									<th class="text-center">Capacity</th>
+									<th class="text-center">Heat</th>
+									<th class="text-center">Water</th>
+									<th class="text-center">Fireplace</th>
+									<th class="text-center">Woodstove</th>
+									<th class="text-center">Fire Ring</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Bear</td>
+									<td>24</td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Brooks</td>
+									<td>14</td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Bushkill</td>
+									<td>32</td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td>Cox</td>
+									<td>14</td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Family Cabin</td>
+									<td>4</td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Firestone</td>
+									<td>40</td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Nuneville</td>
+									<td>20</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td>Trooper</td>
+									<td>20</td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td>Unami</td>
+									<td>26</td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<h2>Campsites</h2>
+					<div class="table-responsive">
+						<table class="table table-sm text-center">
+							<thead class="thead-inverse">
+								<tr>
+									<th class="text-center">Name</th>
+									<th class="text-center">Capacity</th>
+									<th class="text-center">Tent Platforms</th>
+									<th class="text-center">Adirondacks</th>
+									<th class="text-center">Pavilion</th>
+									<th class="text-center">Latrine</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Little Falls - North</td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td>Little Falls - South</td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td>Sportsman Field</td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td>Big Trout</td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td>Blueberry Point</td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td>Lake Roger</td>
+									<td>25</td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Pine Grove</td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td>Bear Hollow</td>
+									<td>35</td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Bear Paw</td>
+									<td>40</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Cayuga</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Cherokee</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Cheyenne</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Chinook</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Chippewa</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Comanche</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Deer Run</td>
+									<td>40</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Eagle's Crest</td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Falcons Roost</td>
+									<td>35</td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Firestone Staff Area</td>
+									<td>35</td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Hillside</td>
+									<td>35</td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Mohawk</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Navajo</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>North Wind</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Porcupine Ridge</td>
+									<td>35</td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Pueblo</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Ruffed Grouse</td>
+									<td>35</td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Running Fox</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Shenandoah</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Shoshone</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Twin Hill 1</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Twin Hill 2</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+								<tr>
+									<td>Two Feathers</td>
+									<td>35</td>
+									<td><i class="fa fa-check"></td>
+									<td></td>
+									<td></td>
+									<td><i class="fa fa-check"></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
 			</div>
 		</div>
 		<?php include '_inc/footer.php'; ?>	
