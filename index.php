2c9d724fad4f80fbba88d873af36aa4494d0ac51
David Gibbons <dgibbns@gmail.com>
2018-03-29 14:42:16 -0400

    Added routes to popular pages

--- index.php
+++ index.php
@@ -14,13 +14,96 @@
 
 		<div class="container content">
 			<div class="row">
-				<div class="col-md-4">
-					<div class="embed-responsive embed-responsive-16by9">
-						<iframe class="embed-responsive-item" src="//www.youtube.com/embed/z7g9fRFGUqE?rel=0" allowfullscreen></iframe>
-					</div>
+				<div class="col-md-6">
+				Situated in the beautiful Pocono Mountains, Resica Falls Scout Reservation is a 4200 acre Scouting Paradise. Set in an endless track of mountain trails, clear streams and waterfalls, Resica has an amazing program of activities for Scouts to enjoy during their summer camp experience. This paradise is the home to hundreds of different trees, plants and animals, and to over 3000 Scouts each summer.
 				</div>
-				<div class="col-md-8">
-					Situated in the beautiful Pocono Mountains, Resica Falls Scout Reservation is a 4200 acre Scouting Paradise. Set in an endless track of mountain trails, clear streams and waterfalls, Resica has an amazing program of activities for Scouts to enjoy during their summer camp experience. This paradise is the home to hundreds of different trees, plants and animals, and to over 3000 Scouts each summer.
+				<div class="col-md-6">
+					<div id="accordion" role="tablist" aria-multiselectable="true">
+						<div class="card">
+							<div class="card-header" role="tab" id="headingFieldDay">
+								<h5 class="mb-0">
+									<a data-toggle="collapse" data-parent="#accordion" href="#collapseFieldDay" aria-expanded="true" aria-controls="collapseFieldDay">
+										Field Day
+									</a>
+								</h5>
+							</div>
+							<div id="collapseFieldDay" class="collapse show" role="tabpanel" aria-labelledby="headingFieldDay">
+								<div class="card-block">
+									<div class="row">
+										<div class="col-md-8">
+											Join us for a day of fun to celebrate Spring in the Poconos April 20-22.
+										</div>
+										<div class="col-md-4">
+											<a class="btn btn-primary btn-block" href="field-day">Learn More</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="headingDocuments">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseDocuments" aria-expanded="false" aria-controls="collapseDocuments">
+										Important Documents
+									</a>
+								</h5>
+							</div>
+							<div id="collapseDocuments" class="collapse" role="tabpanel" aria-labelledby="headingDocuments">
+								<div class="card-block">
+									<div class="row">
+										<div class="col-md-8">
+											Find all of the necessary forms and documents to make your time in camp a success.
+										</div>
+										<div class="col-md-4">
+											<a class="btn btn-primary btn-block" href="important-documents">Learn More</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="headingPrograms">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapsePrograms" aria-expanded="false" aria-controls="collapsePrograms">
+									Programs &amp; Opportunities
+									</a>
+								</h5>
+							</div>
+							<div id="collapsePrograms" class="collapse" role="tabpanel" aria-labelledby="headingPrograms">
+								<div class="card-block">
+									<div class="row">
+										<div class="col-md-8">
+											Check out some of the programs we offer during the summer in camp.
+										</div>
+										<div class="col-md-4">
+											<a class="btn btn-primary btn-block" href="programs-and-opportunities">Learn More</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="headingActivities">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseActivities" aria-expanded="false" aria-controls="collapseActivities">
+									Weekend Activities
+									</a>
+								</h5>
+							</div>
+							<div id="collapseActivities" class="collapse" role="tabpanel" aria-labelledby="headingActivities">
+								<div class="card-block">
+									<div class="row">
+										<div class="col-md-8">
+											Activities are available year-round at camp!
+										</div>
+										<div class="col-md-4">
+											<a class="btn btn-primary btn-block" href="weekend-activities">Learn More</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
