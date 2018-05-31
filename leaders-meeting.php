9d3c0f54bd58830d5f37f7845b598224d753a900
David Gibbons <dgibbns@gmail.com>
2018-05-31 15:58:37 -0400

    Added leaders meeting page

---
+++ leaders-meeting.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<meta name="description" content="Video recap of the pre-camp leaders meeting for Resica Falls Scout Reservation." />
+		<title>Leaders Meeting <?php include '_inc/var/site_name.php'; ?></title>
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="container content">
+			<h1>Leaders Meeting - Online</h1>
+			<div class="card">
+				<div class="row">
+					<div class="hidden-sm-down col-md-3">
+						<a href="https://downloads.resicafalls.org/Video/2018_LeadersMeeting-Online_1.mp4"><img src="_img/leaders-meeting/2018_LeadersMeeting-Online_1.jpg"></a>
+					</div>
+					<div class="col-md-9">
+						<div class="card-block">
+							<h5 class="card-title"><a href="https://downloads.resicafalls.org/Video/2018_LeadersMeeting-Online_1.mp4">Welcome</a></h5>
+							<ul>
+								<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</li>
+								<li>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br>
+			<div class="card">
+				<div class="row">
+					<div class="hidden-sm-down col-md-3">
+						<a href="https://downloads.resicafalls.org/Video/2018_LeadersMeeting-Online_2.mp4"><img src="_img/leaders-meeting/2018_LeadersMeeting-Online_2.jpg"></a>
+					</div>
+					<div class="col-md-9">
+						<div class="card-block">
+							<h5 class="card-title"><a href="https://downloads.resicafalls.org/Video/2018_LeadersMeeting-Online_2.mp4">Food</a></h5>
+							<ul>
+								<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</li>
+								<li>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br>
+			<div class="card">
+				<div class="row">
+					<div class="hidden-sm-down col-md-3">
+						<a href="https://downloads.resicafalls.org/Video/2018_LeadersMeeting-Online_3.mp4"><img src="_img/leaders-meeting/2018_LeadersMeeting-Online_3.jpg"></a>
+					</div>
+					<div class="col-md-9">
+						<div class="card-block">
+							<h5 class="card-title"><a href="https://downloads.resicafalls.org/Video/2018_LeadersMeeting-Online_3.mp4">Program</a></h5>
+							<ul>
+								<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</li>
+								<li>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php include '_inc/footer.php'; ?>
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
