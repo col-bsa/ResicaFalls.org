2559f09b9bbbaf88d4ac5ecbdcae9202e8eb1434
David Gibbons <dgibbns@gmail.com>
2018-07-07 07:14:07 -0400

    Updated downloads page

---
+++ downloads-2018.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<meta name="description" content="Resica Falls Scout Reservation downloads from 2017." />
+		<title>Downloads (2018) <?php include '_inc/var/site_name.php'; ?></title>
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col">
+					<h1>Downloads (2018)</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<div id="accordion1" role="tablist" aria-multiselectable="true">
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download1">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse1" aria-expanded="true" aria-controls="accordion1-collapse1">
+										Week 1: June 24 - July 30
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse1" class="collapse" role="tabpanel" aria-labelledby="accordion1-download1">
+								<div class="card-block">
+									<ul>
+										<li><em>Coming Soon!</em></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download2">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse2" aria-expanded="false" aria-controls="accordion1-collapse2">
+										Week 2: July 1 - July 7
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse2" class="collapse" role="tabpanel" aria-labelledby="accordion1-download2">
+								<div class="card-block">
+									<ul>
+										<li><em>Coming Soon!</em></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download3">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse3" aria-expanded="false" aria-controls="accordion1-collapse3">
+										Week 3: July 8 - July 14
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse3" class="collapse" role="tabpanel" aria-labelledby="accordion1-download3">
+								<div class="card-block">
+									<ul>
+										<li><em>Coming Soon!</em></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download4">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse4" aria-expanded="false" aria-controls="accordion1-collapse4">
+										Week 4: July 15 - July 21
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse4" class="collapse" role="tabpanel" aria-labelledby="accordion1-download4">
+								<div class="card-block">
+									<ul>
+										<li><em>Coming Soon!</em></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download5">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse5" aria-expanded="false" aria-controls="accordion1-collapse5">
+										Week 5: July 22 - July 28
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse5" class="collapse" role="tabpanel" aria-labelledby="accordion1-download5">
+								<div class="card-block">
+									<ul>
+										<li><em>Coming Soon!</em></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download6">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse6" aria-expanded="false" aria-controls="accordion1-collapse6">
+										Week 6: July 29 - August 4
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse6" class="collapse" role="tabpanel" aria-labelledby="accordion1-download6">
+								<div class="card-block">
+									<ul>
+										<li><em>Coming Soon!</em></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="accordion1-download7">
+								<h5 class="mb-0">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion1" href="#accordion1-collapse7" aria-expanded="false" aria-controls="accordion1-collapse7">
+										Week 7: August 5 - August 11
+									</a>
+								</h5>
+							</div>
+							<div id="accordion1-collapse7" class="collapse" role="tabpanel" aria-labelledby="accordion1-download7">
+								<div class="card-block">
+									<ul>
+										<li><em>Coming Soon!</em></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
