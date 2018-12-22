2676ac17200ea75f1f8bd4c8493c32c3acf9a90d
David Gibbons <dgibbns@gmail.com>
2018-12-21 19:00:33 -0500

    Added girls page

---
+++ prepared-for-girls.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<meta name="description" content="We have and we continue to be prepared for girls." />
+		<title>Prepared for Girls <?php include '_inc/var/site_name.php'; ?></title>
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col">
+					<h1>Prepared for Girls</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-7">
+					<p>
+						A few of the ways we have been prepared for girls, and how we continue to work towards the future:
+                        <ul>
+							<li>Female British Scouts and Japanese Venturers, as well as local Sea Scout Ships and Venturing Crews have camped at Resica Falls.</li>
+                            <li>Resica has hired an increasing number of female staff, in both junior staff and camp leadership positions.</li>
+                            <li>Camp improvements that have been made in recent years are female friendly, particularly the 2015 Great Bend Showerhouse &ndash; an entire facility comprised of 10 individual showers/bathrooms.</li>
+                            <li><strong>NEW for 2019!</strong> &ndash; An additional 7 new individual showers/bathrooms located at the Big Springs Pool to accommodate the needs of all our campers.</li>
+                            <li>The camp leadership has developed a well-rounded, action-packed program designed to meet a variety of individual needs.</li>
+                            <li>Check out our Leaders’ Guide, and Program Guide to get an idea of the broad range of activities available at Resica Falls. The 2019 editions will be published in January &amp; February.</li>
+						</ul>
+					</p>
+				</div>
+				<div class="col-md-5">
+					<img class="img-fluid rounded" src="generic/female-staff.jpg" alt="">
+                    <div class="alert alert-brand">
+					    <h5>Don't Miss Out!</h5>
+                        <p>All new female units will receive Tier 1 ($415) pricing for the 2019 season.</p>
+				    </div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col alert alert-brand">
+					<h5 class="text-center">Here are the ways Girls can attend Summer Camp at Resica</h5>
+				</div>
+			</div>
+			<div class="row">
+				
+			</div>
+		</div>
+
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
