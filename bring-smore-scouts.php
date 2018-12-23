00f42fc5a3a720c1fc9ed037fe861154ef4435b9
David Gibbons <dgibbns@gmail.com>
2018-12-22 20:07:02 -0500

    Added smores page

---
+++ bring-smore-scouts.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<meta name="description" content="Make your own Jarassic Adventure this summer at Resica Falls Scout Reservation." />
+		<title>Jurassic Falls <?php include '_inc/var/site_name.php'; ?></title>
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col">
+                    <h1>Bring S'more Scouts</h1>
+				</div>
+            </div>
+            <div class="row">
+				<div class="col-md-5 flex-last">
+					<img class="img-fluid rounded" src="_img/programs/SmoresV1.jpg" alt="">
+				</div>
+                <div class="col-md-7">
+                    <p>A new program for 2019, Bring S’more Scouts looks to entice more of the Scouts in your unit to come to summer camp. If you bring 80% of the registered Scouts in your unit to camp, we'll supply you with all the s'more fixings for a fun night around the campfire in your campsite.</p>
+                    <p>But wait, there’s more &mdash; not only will your unit earn your achievement with s’mores, but you’ll have the opportunity to be joined by one of your favorite staff members.</p>
+                    <p><a href="schedule-a-visit">Sign up today for a camp promotion</a>, and we’ll help you get to 80% and beyond!</p>
+				</div>
+			</div>
+		</div>
+
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
