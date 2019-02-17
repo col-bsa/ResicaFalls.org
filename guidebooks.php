2bb2491540d4325a9c5f8a72a2f5916ce5480701
David Gibbons <dgibbns@gmail.com>
2019-02-17 17:14:42 -0500

    Cleaned up guidebooks page

--- guidebooks.php
+++ guidebooks.php
@@ -14,26 +14,22 @@
 				<div class="col-md-8">
 					<h1>Leaders' Guide &amp; Program Guide</h1>
 					<p>These two guides will serve as your primary resources for planning your stay at camp. They contain valuable information and should be reviewed thoroughly by Scoutmasters, Senior Patrol Leaders, and interested Parents. Many questions can be answered by reading these thorough guides.</p>
-					<p>
-						The <a download href="_files\summer\2019_Leaders_Guide.pdf">Leaders' Guide</a> includes:
-						<ul>
-							<li>Important dates, fees, and times for planning your troop’s stay.</li>
-							<li>Information about camp policies and operations.</li>
-							<li>Guidelines for health &amp; safety, the health lodge, camp emergencies and medications.</li>
-							<li>Details about arrival, check-in, and your first day in camp.</li>
-							<li>Requirements for the Cradle of Liberty Troop Award and the Resica Falls Patrol Award.</li>
-						</ul>
-					</p>
-					<p>
-						The <a download href="_files\summer\2019_Program_Guide.pdf">Program Guide</a> includes:
-						<ul>
-							<li>Merit Badge pre-requisites, location, schedules, and any associated fees.</li>
-							<li>Details about campwide programs and troop &amp; patrol activities.</li>
-							<li>Information about our first year camper program – the Resica Rangers.</li>
-							<li>Facts relating to High Adventure and Aquatics programs.</li>
-							<li>Highlights of the unique opportunities and troop trips offered at Resica Falls.</li>
-						</ul>
-					</p>
+					The <a download href="_files\summer\2019_Leaders_Guide.pdf">Leaders' Guide</a> includes:
+					<ul>
+						<li>Important dates, fees, and times for planning your troop’s stay.</li>
+						<li>Information about camp policies and operations.</li>
+						<li>Guidelines for health &amp; safety, the health lodge, camp emergencies and medications.</li>
+						<li>Details about arrival, check-in, and your first day in camp.</li>
+						<li>Requirements for the Cradle of Liberty Troop Award and the Resica Falls Patrol Award.</li>
+					</ul>
+					The <a download href="_files\summer\2019_Program_Guide.pdf">Program Guide</a> includes:
+					<ul>
+						<li>Merit Badge pre-requisites, location, schedules, and any associated fees.</li>
+						<li>Details about campwide programs and troop &amp; patrol activities.</li>
+						<li>Information about our first year camper program – the Resica Rangers.</li>
+						<li>Facts relating to High Adventure and Aquatics programs.</li>
+						<li>Highlights of the unique opportunities and troop trips offered at Resica Falls.</li>
+					</ul>
 				</div>
 				<div class="col-md-4">
 					<img class="img-fluid rounded" src="_img/generic/2019_ResicaGuides.png" alt="">
