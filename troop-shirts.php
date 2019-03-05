209e26a518cb42acc709f63510616b8b8c653dd0
David Gibbons <dgibbns@gmail.com>
2019-03-04 23:30:30 -0500

    Added campership addplication

--- troop-shirts.php
+++ troop-shirts.php
@@ -11,12 +11,16 @@
 
 		<div class="container content">
 			<div class="row">
-				<div class="col-md-6 col-lg-7">
+				<div class="col">
 					<h1>Resica Troop T-Shirt Program</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-lg-7">
 					<p>We are proud to once again provide the Troop T-Shirt Program to units for the Resica Summer Camp season. The shirt highlights this year’s theme, Jurasic Falls with a full chest design on a high-quality, heavy-duty light blue shirt. By signing up as a unit with at least 10 shirts, your unit’s number and hometown will be added to the back.</p>
 					<p>Order your shirts by the deadlines above and they will be packaged and delivered when you arrive in camp, with no extra hassles or shipping fees.</p>
 					<div class="row">
-						<div class="col-md-6">
+						<div class="col-lg-6">
 							<h2>Prices</h2>
 							<ul>
 								<li>S-XL: $15.00</li>
@@ -24,7 +28,7 @@
 								<li>3XL-5XL: $21.00</li>
 							</ul>
 						</div>
-						<div class="col-md-6">
+						<div class="col-lg-6">
 							<h2>Order Now</h2>
 							<a class="btn btn-primary btn-block" href="http://colbsa.doubleknot.com/event/2019-resica-falls-summer-camp-troop-tshirt-weeks-1-3-only/2491365" target="_blank">Weeks 1-3</a>
 							<a class="btn btn-primary btn-block" href="http://colbsa.doubleknot.com/event/2019-resica-falls-summer-camp-troop-tshirt-weeks-4-7-only/2491368" target="_blank">Weeks 4-7</a>
