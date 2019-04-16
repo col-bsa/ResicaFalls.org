c4903228d438a06a370b5683cebb8e33ee844f9f
Matt Gutterman <mgutterman@unamilodge.org>
2019-04-16 17:18:12 -0400

    Removed FLIK references from food page

--- food-and-nutrition.php
+++ food-and-nutrition.php
@@ -18,16 +18,16 @@
 			<div class="row">
 				<div class="col-md-8">
 					<p>
-						Resica Falls has partnered with FLIK Independent School Dining to provide Scouts and Scouters with a well-balanced and nutritious meal plan- approved by a certified nutritionist- for the duration of their stay in camp, beginning with dinner on Sunday. Each meal includes a hot entr&eacute;e with at least one side; lunch and dinner also include a vegetable. There is a fruit and yogurt bar available at breakfast, and a salad bar at both lunch and dinner, along with daily specialty salad bars at lunch. Peanut butter and jelly with multiple types of bread are also available at both lunch and dinner. Hot and cold drinks are available at twin juice bars located on either side of the dining hall.
+						Resica Falls has partnered with a food service management company to provide Scouts and Scouters with a well-balanced and nutritious meal plan- approved by a certified nutritionist- for the duration of their stay in camp, beginning with dinner on Sunday. Each meal includes a hot entr&eacute;e with at least one side; lunch and dinner also include a vegetable. There is a fruit and yogurt bar available at breakfast, and a salad bar at both lunch and dinner, along with daily specialty salad bars at lunch. Peanut butter and jelly with multiple types of bread are also available at both lunch and dinner. Hot and cold drinks are available at twin juice bars located on either side of the dining hall.
 					</p>
-					<h2>Menu</h2>
+					<!-- <h2>Menu</h2>
 					<ul>
 						<li><a target="_blank" href="_files/summer/2018_Summer_Menu-OddWeeks.pdf">Weeks 1,3,5,7</a></li>
 						<li><a target="_blank" href="_files/summer/2018_Summer_Menu-EvenWeeks.pdf">Weeks 2,4,6</a></li>
 						<li><a target="_blank" href="http://colbsa.doubleknot.com/survey/2018-summer-camp-special-diet-request/48849">2018 Summer Camp Special Diet Request</a></li>
-					</ul>
+					</ul> -->
 					<p>
-						Campers and adult leaders with dietary restrictions can note their individual restrictions with the camp’s FLIK food service manager before they come to camp. FLIK is more than happy to accommodate any dietary restrictions to ensure that campers and leaders have the best dining experience possible. For more information, or to discuss dietary restrictions, please contact Marco Morales, FLIK regional manager, at <a href="mailto:MARCO.MORALES@COMPASS-USA.COM">marco.morales@compass-usa.com</a>.
+						Campers and adult leaders with dietary restrictions can note their individual restrictions with the camp's food service manager before they come to camp. We are more than happy to accommodate any dietary restrictions to ensure that campers and leaders have the best dining experience possible.
 					</p>
 				</div>
 				<div class="col-md-4">
