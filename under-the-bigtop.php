762f7227da46c0a8c153cc38774d5bd290591dde
David Gibbons <dgibbns@gmail.com>
2019-10-17 16:22:52 -0400

    Made changes as requested

--- under-the-bigtop.php
+++ under-the-bigtop.php
@@ -19,6 +19,8 @@
             <div class="row">
 				<div class="col-md-4 flex-last">
 					<img class="img-fluid rounded" src="_img/programs/theme/RF-UnderTheBigTop.jpg" alt="">
+					<br><br>
+					<a class="btn btn-success" href="dates-and-rates">See Dates &amp; Rates</a>
 				</div>
                 <div class="col-md-8">
 					<p>The house lights are up and the circus is in town for the adventure that awaits with our 2020 theme, "Under the Big Top!" Your unit will be treated to a display of marvels during your stay at Resica Falls.</p>
@@ -33,7 +35,7 @@
             <div class="row">
 				<div class="col text-center">
                     <h4>
-                        Find out more details in January - February 2020
+                        Find out more details in January / February 2020
                         <br>
                         when our Program and Leaders' Guide is released!
                     </h4>
