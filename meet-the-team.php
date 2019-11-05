8ab481e1a1c08d84346c599573a47f26dcd2dee7
David Gibbons <dgibbns@gmail.com>
2019-11-05 11:33:42 -0500

    Updated meet the team profiles

--- meet-the-team.php
+++ meet-the-team.php
@@ -22,7 +22,7 @@
 				<div class="col-12 col-md-10">
 					<h2>Chris Brenner <div class="h5">Camp Director</div></h2>
 					<p>
-					This marks Chris's 14th summer as the Camp Director at Resica Falls. An Eagle Scout and Vigil Honor member of Unami Lodge, Chris has many years of Scouting experience, including 15 years as a Scoutmaster, various work on district and council committees, and one summer as Camp Director at Treasure Island. A retired school teacher of 35 years, Chris's expert leadership has become a standard of Resica Falls. Chris's favorite candy is Swedish Fish. Contact Chris at <a href="mailto:campdirector@resicafalls.org">campdirector@resicafalls.org</a>.
+					This marks Chris's 15th summer as the Camp Director at Resica Falls. An Eagle Scout and Vigil Honor member of Unami Lodge, Chris has many years of Scouting experience, including 15 years as a Scoutmaster, various work on district and council committees, and one summer as Camp Director at Treasure Island. A retired school teacher of 35 years, Chris's expert leadership has become a standard of Resica Falls. Chris's favorite candy is Swedish Fish. Contact Chris at <a href="mailto:campdirector@resicafalls.org">campdirector@resicafalls.org</a>.
 					</p>
 				</div>
 			</div>
@@ -33,7 +33,7 @@
 				<div class="col-12 col-md-10">
 					<h2>Steve Mach <div class="h5">Assistant Camp Director</div></h2>
 					<p>
-					Steve is in his 11th summer at Resica Falls, and his fourth as Assistant Camp Director. Previously, he has worked as Handicraft Director, Commissioner, and Program Director. He is also an Eagle Scout and Vigil Honor member of Unami Lodge, One. A high school social studies teacher, Steve is always looking for ways to make camp better. Steve's favorite color is green. Contact Steve at <a href="mailto:asstcampdirector@resicafalls.org">asstcampdirector@resicafalls.org</a>.
+					Steve is in his 12th summer at Resica Falls, and his fifth as Assistant Camp Director. Previously, he has worked as Handicraft Director, Commissioner, and Program Director. He is also an Eagle Scout and Vigil Honor member of Unami Lodge, One. A high school social studies teacher, Steve is always looking for ways to make camp better. Steve's favorite color is green. Contact Steve at <a href="mailto:asstcampdirector@resicafalls.org">asstcampdirector@resicafalls.org</a>.
 					</p>
 				</div>
 			</div>
@@ -44,7 +44,7 @@
 				<div class="col-12 col-md-10">
 					<h2>Bobby Burkhart <div class="h5">Program Director</div></h2>
 					<p>
-					Bobby joins the leadership team this summer as the Program Director. This is his 8th summer on camp staff, serving primarily on Aquatics staff. He is an Eagle Scout and Vigil Honor member of Unami Lodge, One, and works as a special education teacher. Bobby is filled with energy and enthusiasm for camp. Bobby enjoys telling "Dad jokes" in his free time. Contact Bobby at <a href="mailto:programdirector@resicafalls.org">programdirector@resicafalls.org</a>.
+					Bobby returns to the leadership team for his second summer Program Director. This is his 9th summer on camp staff, serving primarily on Aquatics staff. He is an Eagle Scout and Vigil Honor member of Unami Lodge, One, and works as a special education teacher. Bobby is filled with energy and enthusiasm for camp. Bobby enjoys telling "Dad jokes" in his free time. Contact Bobby at <a href="mailto:programdirector@resicafalls.org">programdirector@resicafalls.org</a>.
 					</p>
 				</div>
 			</div>
@@ -55,7 +55,7 @@
 				<div class="col-12 col-md-10">
 					<h2>Andy Connell <div class="h5">Assistant Program Director</div></h2>
 					<p>
-					Andy brings 8 years of camp staff experience to the table in his first summer as Assistant Program Director. This Eagle Scout has previously worked as Civics Director and Program Commissioner. Andy is an integral part of the success of Resica Falls. Andy's favorite board game is Clue. Contact Andy at <a href="mailto:asstprogramdirector@resicafalls.org">asstprogramdirector@resicafalls.org</a>.
+					Andy brings 9 years of camp staff experience to the table in his second summer as Assistant Program Director. This Eagle Scout has previously worked as Civics Director and Program Commissioner. Andy is an integral part of the success of Resica Falls. Andy's favorite board game is Clue. Contact Andy at <a href="mailto:asstprogramdirector@resicafalls.org">asstprogramdirector@resicafalls.org</a>.
 					</p>
 				</div>
 			</div>
@@ -66,7 +66,7 @@
 				<div class="col-12 col-md-10">
 					<h2>Jerry Reed <div class="h5">Business Manager</div></h2>
 					<p>
-						Jerry brings over 40 years of camp experience to the table as he oversees the administrative operations of camp. Previously, this Vigil Honor member of Unami Lodge, One has worked as a Program Commissioner, Evening Program Coordinator, Program Director, COPE Director, Assistant Camp Director and Camp Director at three different camps – Tri-Mount, Barton, and Resica Falls. Jerry's favorite spice is salt. Contact Jerry at <a href="mailto:businessmanager@resicafalls.org">businessmanager@resicafalls.org</a>.
+						Jerry brings over 45 years of camp experience to the table as he oversees the administrative operations of camp. Previously, this Vigil Honor member of Unami Lodge, One has worked as a Program Commissioner, Evening Program Coordinator, Program Director, COPE Director, Assistant Camp Director and Camp Director at three different camps &mdash; Tri-Mount, Barton, and Resica Falls, as well as on the faculty of many National Camping Schools. Jerry's favorite spice is salt. Contact Jerry at <a href="mailto:businessmanager@resicafalls.org">businessmanager@resicafalls.org</a>.
 					</p>
 				</div>
 			</div>
@@ -77,7 +77,18 @@
 				<div class="col-12 col-md-10">
 					<h2>Nick McKee <div class="h5">Ranger</div></h2>
 					<p>
-						This is Nick's fifth summer as the Ranger at Resica Falls. An Eagle Scout and Vigil Honor member, Nick has worked in many different areas of camp over the past 16 years, including Handicraft, Dining Hall, QM, and spending two years as the Assistant Ranger at Musser Scout Reservation. Nick's work ethic has been vital to camp since his return to Resica in 2015. Nick's favorite food is bear meat. Contact Nick at <a href="mailto:nicholas.mckee@scouting.org">nicholas.mckee@scouting.org</a>. 
+						This is Nick's sixth summer as the Ranger at Resica Falls. An Eagle Scout and Vigil Honor member, Nick has worked in many different areas of camp over the past 17 years, including Handicraft, Dining Hall, QM, and spending two years as the Assistant Ranger at Musser Scout Reservation. Nick's work ethic has been vital to camp since his return to Resica in 2015. Nick's favorite food is bear meat. Contact Nick at <a href="mailto:nicholas.mckee@scouting.org">nicholas.mckee@scouting.org</a>.
+					</p>
+				</div>
+			</div>
+			<div class="row split">
+				<div class="hidden-sm-down col-md-2">
+					<img class="img-fluid rounded" src="_img/profiles/john.jpg" alt="">
+				</div>
+				<div class="col-12 col-md-10">
+					<h2>John Bickel <div class="h5">Director of Outdoor Adventures</div></h2>
+					<p>
+						John is an Eagle Scout and Vigil Honor member who previously served as the Dan Beard Director, Head Commissioner, and Program Director at Resica Falls. After two summers as the Camp Director at Musser Scout Reservation, this former Lodge Chief took a break from our camping operations, while also continuing to serve on National Camping School staff. A professional scouter, John has served the Cradle of Liberty Council in a variety of positions, including district executive, council program director, and senior development director. John's favorite football team is the Philadelphia Eagles. Contact John at <a href="mailto:john.bickel@scouting.org">john.bickel@scouting.org</a>.
 					</p>
 				</div>
 			</div>
