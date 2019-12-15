d2ed8315d64a72fe490deba27b8db7e9fc5f0836
David Gibbons <dgibbns@gmail.com>
2019-12-15 18:52:46 -0500

    Re-evaluated my ability to count

--- field-weekend.php
+++ field-weekend.php
@@ -18,7 +18,7 @@
             </div>
             <div class="row">
                 <div class="col-md-8">
-                    <p>Set aside the date, inspect your gear, get your recipes ready and plan to have lots of fun when you join us for the 2nd Annual Resica Field Weekend on April 24-26, 2020.</p>
+                    <p>Set aside the date, inspect your gear, get your recipes ready and plan to have lots of fun when you join us for the 3rd Annual Resica Field Weekend on April 24-26, 2020.</p>
                     <p>The Field Weekend allows your Troop to hone their tent camping skills while taking advantage of the great activities and facilities at Resica Falls Scout Reservation.</p>
                     <p>Some of the events include the older Scout Cowboy Action Shooting as well as traditional Scouts BSA activities like lashing, orienteering, Dutch Oven cooking, fishing, and climbing.</p>
                     <p>One of the highlight competitions will be the Iron Chef Event, a Dutch Oven cooking event centered around a “secret ingredient”. We will provide the “secret ingredient” to each team. The event will run the last 3.5 hours of the day and conclude when the competing troops/crews/patrols provides 2 dishes to the Council Executives for judging.</p>
