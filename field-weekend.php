4e4e2d6269cf3b385f5a6d009f4b23f21b5f81f0
David Gibbons <dgibbns@gmail.com>
2020-01-24 18:53:30 -0500

    Added Staff registration link and removed the 2018 Leaders Guide link

--- field-weekend.php
+++ field-weekend.php
@@ -21,10 +21,11 @@
                     <p>Set aside the date, inspect your gear, get your recipes ready and plan to have lots of fun when you join us for the 3rd Annual Resica Field Weekend on April 24-26, 2020.</p>
                     <p>The Field Weekend allows your Troop to hone their tent camping skills while taking advantage of the great activities and facilities at Resica Falls Scout Reservation.</p>
                     <p>Some of the events include the older Scout Cowboy Action Shooting as well as traditional Scouts BSA activities like lashing, orienteering, Dutch Oven cooking, fishing, and climbing.</p>
-                    <p>One of the highlight competitions will be the Iron Chef Event, a Dutch Oven cooking event centered around a “secret ingredient”. We will provide the “secret ingredient” to each team. The event will run the last 3.5 hours of the day and conclude when the competing troops/crews/patrols provides 2 dishes to the Council Executives for judging.</p>
+                    <p>One of the highlight competitions will be the Iron Chef Event, a Dutch Oven cooking event centered around a “secret ingredient”. We will provide the "secret ingredient" to each team. The event will run the last 3.5 hours of the day and conclude when the competing troops/crews/patrols provides 2 dishes to the Council Executives for judging.</p>
                     <p>Check back here as details are finalized.</p>
-                    <a class="btn btn-primary btn-block" href="_files/special_programs/2018_FieldDay_LeadersGuide.pdf" download>Download the 2018 Leaders' Guide</a>
+                    <p>Stay tund for the 2020 Leaders' Guide!</p>
 					<a class="btn btn-success btn-block" href="https://colbsa.doubleknot.com/event/2020-field-event-weekend-resica-falls/2600939" target="_blank">Register Now</a>
+                    <a class="btn btn-success btn-block" href="https://colbsa.doubleknot.com/event/2020-resica-falls-field-day-staff-sign-up-only/2616651" target="_blank">Sign up for Staff</a>
                     <br>
                 </div>
 				<div class="col-md-4">
