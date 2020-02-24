59bcf6a08246fcf655adaacd4fd96c3ebd300cd3
David Gibbons <dgibbns@gmail.com>
2020-02-24 11:09:42 -0500

    Added Senior and Junior ATV Program distinctions

--- atv-program.php
+++ atv-program.php
@@ -21,10 +21,21 @@
 			<div class="row">
 				<div class="col">
 					<p>
-						In the summer of 2016 Resica Falls premiered its newest and most exciting program area: the ATV course! Presented through a partnership between the Boy Scouts of America and Polaris Industries Inc., Scouts can spend a week learning the fundamentals of riding under the supervision of an ATV Safety Institute-certified instructor. Scouts begin riding on day one, and by Friday they’ll be riding the reservation like pros! Scouts who successfully complete the program will receive a certificate and an ATV <em>RiderCourse</em>&trade; certification card from the ATV Safety Institute.
+						In the summer of 2016 Resica Falls premiered its newest and most exciting program area: the ATV course! Presented through a partnership between the Boy Scouts of America and Polaris Industries Inc., Scouts can spend a week learning the fundamentals of riding under the supervision of an ATV Safety Institute-certified instructor. For the 2020 season, we're changing how we offer our ATV program, and are offering two different experiences.
 					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h3>Junior ATV Program</h3>
 					<p>
-						In order to participate in the ATV program, Scouts must be 16 and older, weigh 225 lbs or less, and have a parent's permission. Scouts must also have a current BSA Annual Health & Medical Record signed by a physician within the last 12 months. The program costs an additional $25 beyond the normal summer camp fee.
+						Offered during two afternoon, double-period sessions: 2:00pm-3:15pm and 3:30pm-4:45pm. Scouts participating in this program must be at least 14 years of age or older. Each session is limited to 6 Scouts, and there is a 215 lb weight limit on the ATVs. Those participating must complete an online blended-learning course found at <a href="http://atvsafety.org/atv-ecourse" target="_blank">atvsafety.org/atv-ecourse</a>. The course will take approx. 2 hours to complete. Scouts must bring a certificate of completion for this online course, a current BSA Annual Health & Medical Record, and an ATV Participation and Hold-Harmless Agreement signed by their parent or guardian. The program costs an additional $25 beyond the normal summer camp fee. Scouts who successfully complete the program will receive a patch, certificate, and an ATV RiderCourse&trade; certification card from the ATV Safety Institute.
+					</p>
+				</div>
+				<div class="col-md-6">
+					<h3>Senior ATV Program</h3>
+					<p>
+						Offered during two morning, single-period sessions: 9:00am-9:45am and 10:00am-10:45am. Scouts participating in this program must be at least 16 years of age or older. Each session is limited to 8 Scouts, and there is a 215 lb weight limit on the ATVs. Those participating must complete an online blended-learning course found at <a href="http://atvsafety.org/atv-ecourse" target="_blank">atvsafety.org/atv-ecourse</a>. The course will take approx. 2 hours to complete. Scouts must bring a certificate of completion for this online course, a current BSA Annual Health & Medical Record, and an ATV Participation and Hold-Harmless Agreement signed by their parent or guardian. The program costs an additional $25 beyond the normal summer camp fee. Scouts who successfully complete the program will receive a patch, certificate, and an ATV RiderCourse&trade; certification card from the ATV Safety Institute.
 					</p>
 				</div>
 			</div>
