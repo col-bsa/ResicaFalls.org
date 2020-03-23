b57fb7331da07c4d52a9ed2d5aacbd08be4fccc3
David Gibbons <dgibbns@gmail.com>
2020-03-23 10:52:18 -0400

    Updated campership information

--- campership.php
+++ campership.php
@@ -15,7 +15,7 @@
 		<div class="container content">
 			<div class="row">
 				<div class="col">
-					<h1>2019 Campership Program</h1>
+					<h1>2020 Campership Program</h1>
 				</div>
 			</div>
 			<div class="row">
@@ -23,8 +23,8 @@
 					<h2>Purpose</h2>
 					<p>To provide supplemental funding for a registered Scout of the Cradle of Liberty Council attending a Cradle of Liberty Council summer camp, whose parents/guardians and unit leader certify is unable to pay the total cost. Further they acknowledge that the Scout is deserving of the award and sincerely interested in attending summer camp. Funding is limited, and we ask only those with true needs apply for aid – our assessment criteria is similar to the Federal Income Eligibility Guidelines. Youth members are expected to earn their own way in Scouting and each unit should provide an opportunity for Scouts to participate in fundraising activities to help offset these costs. </p>
 					<h2>Important Dates</h2>
-					<p>ALL applications MUST be received by April 1st. No money needs to be submitted with the application. Applications received after this date will only be considered IF funds become available.</p>
-					<p>Letters to both parents and unit leaders will be mailed out by April 25. Acceptance of the award and the camp deposit ($50 for Resica Falls and $30 for Cub Adventures at Musser) must be received at the council office by May 15. All camp fees (less the amount awarded) must be paid in full BEFORE the campership is applied directly to the Scout’s camp fee. Unit leaders will be able to see this in the online registration system. Unit leaders should ensure that the Scout is added to their camp roster in the online registration system in order that the financial aid can be applied.</p>
+					<p>ALL applications MUST be received by April 15th. No money needs to be submitted with the application. Applications received after this date will only be considered IF funds become available.</p>
+					<p>Letters to both parents and unit leaders will be mailed out by May 4th. Acceptance of the award and the camp deposit ($50 for Resica Falls and $30 for Cub Adventures at Musser) must be received at the council office by May 15. All camp fees (less the amount awarded) must be paid in full BEFORE the campership is applied directly to the Scout’s camp fee. Unit leaders will be able to see this in the online registration system. Unit leaders should ensure that the Scout is added to their camp roster in the online registration system in order that the financial aid can be applied.</p>
 					<p>If the request is denied, or the amount of the grant is less than requested the applicant will have until the unit’s camp date to pay the balance of fees without penalty. Please note if paying at camp then the letter stating the amount of financial aid granted must be taken to camp so that the award can be applied to the Scout’s account/unit account correctly.</p>
 					<h2>Cubmasters and Webelos Leaders</h2>
 					<p>Current 5th graders who are crossing over to Scouts BSA units should be advised to make arrangements to attend summer camp with the troop they intend to join. Being new to a troop is NOT a valid reason to apply for financial aid, and the troop may be going to an out of council camp which would not qualify them for financial assistance from Cradle of Liberty Council.</p>
@@ -41,8 +41,7 @@
 					<img class="img-fluid" src="_img/generic/flag-raising.jpg" alt="">
 					<br><br>
 					<h2>Downloads</h2>
-					<a class="btn btn-primary btn-block" href="_files/summer/CampFinancialAidApplication2019.pdf" download>Campership Application</a>
-					<a class="btn btn-primary btn-block" href="_files/summer/CampFinancialAidApplication2019(fillable).pdf" download>Campership Application (Fillable)</a>
+					<a class="btn btn-primary btn-block" href="_files/summer/2020_Campership_Application.pdf" download>Campership Application</a>
 				</div>
 			</div>
 		</div>
