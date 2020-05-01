65237998ad9254b7c375d94b8dfdf33de2926380
David Gibbons <dgibbns@gmail.com>
2020-05-01 15:41:41 -0400

    Updated coronavirus update

--- coronavirus.php
+++ coronavirus.php
@@ -16,89 +16,57 @@
 			<div class="row">
 				<div class="col">
 					<h1>Response to COVID-19</h1>
-                    <h4>Updated April 6, 2020</h4>
+                    <h4>Updated May 1, 2020</h4>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-8">
-					<p>
+                <p>
                     Dear Friends of Resica Falls,
-					</p>
-					<p>
-                    We hope everyone is healthy and looking forward to another exciting summer camp experience at Resica Falls. As we experience this beautiful day across Eastern Pennsylvania today, we are reminded that summer is indeed on its way! As we continue to prepare for camp, we wanted to share an update so everyone is prepared with the latest information.
-					</p>
+                    </p>
+
                     <p>
-                    Although still more than two months away, the topic that we field the most questions about is the operation of summer camp and how we are handling the many facets and deadlines involved in a successful summer camp. </strong>When answering this question, we find ourselves wanting to be both hopeful and realistic as we communicate the uncertainty around the situation.</strong>
-					</p>
+                    For most Scouts, summer camp is the highlight of the Scouting year. With that in mind, key volunteers across several council committees, along with the summer camp and council leadership teams, have spent a tremendous amount of time and effort monitoring updates and projections by several government, Scouting, and camping organizations from the local to national levels. The goal was to seek a path forward to be able to conduct our summer programs in a manner that will keep our Scouts and leaders safe, maintaining our council’s unwavering commitment to the well-being of our members. 
+                    </p>
+
                     <p>
-                    <strong>We know that our Scouts look forward to their summer adventures in the outdoors. We want to provide optimism for an exciting summer, while also being responsible and setting realistic expectations. As of today, April 6, 2020, we are planning for a normal summer operation!</strong> Here is how we continue to evaluate the situation to make the best decisions about our summer operations:
-					</p>
-                    <ol>
-                        <li>
-                        <strong>Safety is paramount</strong> &mdash; We will not do anything to jeopardize the safety of our Scouts, families or the safety of our first responders and front-line medical personnel. 
-                        </li>
-                        <li>
-                        <strong>Sound Decision-making</strong> &mdash; We have a great leadership core both in our council and the community in-general. Decisions are made with all stakeholders in-mind and using the latest information.
-                        </li>
-                        <li>
-                        <strong>Delivering the Promise</strong> &mdash; As of today, we are preparing and are hopeful that we will be able to safely operate our camps this summer. Prepare for an experience “Under the Big Top” and start planning your costumes and gateways! However, we recognize this could change based on information and directives from federal, state or local officials. If it does, we will communicate quickly and thoroughly. Should no further directives come over the course of the next two weeks, <strong>you can expect our next formal update and communication on Monday, April 20, 2020.</strong>
-                        </li>
-                    </ol>
+                    Regrettably, we have come to the difficult decision to cancel all in-person summer camp operations for 2020. This decision was not arrived at lightly, but it is aligned with and supports the council’s ongoing commitment to the safety of our Scouts and leaders above all else. Knowing how important an outdoor experience is to our youth, we will continue to monitor state and local guidance with the hope of being able to offer small-group gatherings later in the summer.
+                    </p>
+
                     <p>
-                    Even though we continue to prepare for camp, we realize that there is a chance that camp might not go on as planned. Even so, we are encouraging units and families to continue signing up and submitting payments. <em>As a reminder, final payments are due May 15th.</em>
-					</p>
-                    <p>
-                    Some of you might ask <em>"why should we continue to pay for camp".</em> There are two important reasons to do so:
-					</p>
-                    <ol>
-                        <li>
-                        <strong>Planning</strong> &mdash; Most importantly, we need this information for planning purposes such as budgeting, staffing, ordering (eventually), etc. This allows us to have an understanding for what to anticipate this summer.
-                        </li>
-                        <li>
-                        <strong>Contingencies</strong> &mdash; If we decided today to close camps and then learned that it is safe to operate camps this summer, it would be very difficult to do so at the last minute. Continuing to register and pay for camp lets us know that you are as excited as we are for a summer camp experience, even if that has to look different than normal.
-                        </li>
-                    </ol>
-                    <p>
-                    With our units and families in mind, here are a few updates we wanted to share:
-					</p>
+                    Knowing how important a summer program is for your Scouts, we will be offering a new type of camp program this summer to continue to provide the same Scout-oriented experience that is synonymous with Resica Falls. <strong>2020 Virtual Camp @ Resica Falls</strong> will be a whole new level of adventure that we are excited to be able to offer to our Scouts and units this summer. We are here for you. While many finer details are still being developed, there are some things we can tell you right now:
+                    </p>
                     <ul>
-                        <li>
-                        <strong>Campership Deadline</strong> &mdash; Recognizing that families are under sudden financial hardship, we have extended the Campership Deadline from April 1 to April 15, 2020 for Cradle of Liberty Council families.
-                        </li>
-                        <li>
-                        <strong>Refund Policy</strong> &mdash; We have adopted a "full and hassle-free refund policy". If camp should not be able to run as planned or if a unit or family decides to not participate by May 31st, all monies will be returned through the vehicle they came in (i.e. your unit, your family). Troops wishing to roll their unit deposit to 2021 can do so.
-                        </li>
-                        <li>
-                        <strong>Medicals/Physicals</strong> &mdash; We continue to monitor guidance from the National Council regarding the Annual Health and Medical Record and will take their direction if new standards are outlined due to the difficulty in scheduling an appointment for a physical. We are also working with our Health and Safety Committee to make physicals available to campers if needed.
-                        </li>
-                        <li>
-                        <strong>Pre-Camp Leaders Meeting</strong> &mdash; We will hold one virtual pre-camp leaders meeting this year on Monday, May 4th. Information regarding platform will be provided to leaders as the meeting approaches. The meeting will be recorded for future viewing if you aren’t able to attend.
-                        </li>
-                        <li>
-                        <strong>Cleanliness & Safety</strong> &mdash; We have always done our best to have a clean camp, and this year will be no different. In addition to our regular cleanings, we will have more hand washing stations throughout camp and encourage campers to wash their hands regularly. We are also looking at new best-practices to enhance our incoming medical re-checks to ensure all campers in camp are healthy upon arrival.
-                        </li>
+                        <li>We will still be following our theme of “Under the Big Top,” and many of our camp-wide activities will focus on this theme.</li>
+                        <li>Scouts will have the opportunity to take up to 6 merit badges throughout the week and receive instruction from our specially-trained counselors.</li>
+                        <li>Extensive camp-wide programs will be offered, from lunch clubs, to camp-wide games, along with a Scout’s Own Service, an OA Ice Cream Social, and of course – opening & closing campfires.</li>
+                        <li>Every Scout will receive a welcome packet in the mail with all the necessary program supplies to complete the badges they choose and to be a part of our camp experience.</li>
+                        <li>We will be offering extensive programs and training for adult leaders as well!</li>
                     </ul>
+
                     <p>
-                    <strong>The Resica Falls Leadership Team is planning for and committed to providing exceptional experiences to our Scouts.</strong> If we are unable to provide those in the typical way this summer, we will quickly pivot and find new and innovative ways to offer the program.
-					</p>
+                    The staff is working hard to lock in details and we will email you updates as they become available. Unit leaders can learn more about Virtual Camp by attending the Pre-Camp Leaders Meeting on Monday, May 4th at 7pm. <a href="https://us02web.zoom.us/webinar/register/WN_krch0GiMQYGhaTsbc1q7HQ" target="_blank">Click this link to register to join us.</a>
+                    </p>
+
                     <p>
-                    Speaking of innovative efforts - if you and your Scouts have yet to take advantage of the virtual resources the Cradle of Liberty Council is currently offering, we encourage you to visit <a href="https://colbsa.org/homescouting" target="_blank">colbsa.org/homescouting</a> for advancement, entertainment, and activities around the council. Follow <a href="https://facebook.com/resicafalls">Resica Falls</a> and <a href="https://facebook.com/colbsa">Cradle of Liberty Council</a> on Facebook to receive the most recent communications on new programs as they are available.
-					</p>
+                    We will be answering many questions during the Pre-Camp Leader’s Meeting on Monday, and a full FAQ will be posted to www.resicafalls.org/coronavirus over the weekend. For additional questions, program questions, please reach-out to <a href="mailto:campdirector@resicafalls.org">Chris Brenner at campdirector@resicafalls.org</a>, and if you have refund questions, please reach-out to <a href="mailto:tracy.launders@scouting.org">Tracy Launders at tracy.launders@scouting.org</a>. For general questions about Cradle of Liberty Camping operations, feel free to reach-out to <a href="mailto:john.bickel@scouting.org">John Bickel at john.bickel@scouting.org</a> or (484) 614-2370.
+                    </p>
+
                     <p>
-                    <strong>We continue to pray for the health and safety of our Scouting families, as well as all those on the front-lines in harm’s way.</strong> We appreciate all that you have done and continue to do as a partner in delivering the promises of Scouting to your families. Continue to stay healthy, and we will continue to update you regularly as the situation progresses.
-					</p>
+                    While this is not how any of us expected the summer of 2020 to go, the volunteers and staff of the Cradle of Liberty Council remain committed to providing programs to help you and your Scouts make the most of these unprecedented times. We remain hopeful that we will be able to provide small-group programs later in the summer as government guidelines allows, and we eagerly look ahead to the incredible line up of fall programs that are planned! Until then, please stay healthy and safe and we hope to see you soon along the virtual trail.  
+                    </p>
+
                     <p>
                     Yours in Scouting,
                     </p>
-                    <p class="h4">
-                    <em>Chris, Steve, Bobby, Jerry, Andy, and Nick</em>
-                    </p>
                     <p>
-                    2020 Resica Falls Leadership Team
-					</p>
+                    Dan Templar
+                    Scout Executive & CEO
+                    Cradle of Liberty Council, BSA
+                    </p>
 				</div>
 				<div class="col-md-4">
-                    <a class="btn btn-success btn-block" href="/_files\summer\2020.04.06_Covid-19-Update.pdf" download>Download this Letter</a>
+                    <a class="btn btn-success btn-block" href="/_files\summer\2020.04.06_Covid-19-Update.pdf" download>April 6, 2020 Update</a>
                     <a class="btn btn-success btn-block" href="/_files\summer\2020.03.20_Covid-19-Update.pdf" download>March 20, 2020 Update</a>
 					<a class="btn btn-primary btn-block" href="https://colbsa.org/coronavirus" target="_blank">Cradle of Liberty Council Response</a>
 				</div>
