cee360492b08f68356eafa67e49ebbb2b1d93e77
David Gibbons <dgibbns@gmail.com>
2020-05-20 08:32:02 -0400

    added trading post under construction page

---
+++ tradingpost.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<meta name="description" content="The team at Resica Falls Scout Reservation is still working on this page. Please check back again later!" />
+		<title>Trading Post Coming Soon <?php include '_inc/var/site_name.php'; ?></title>
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5R8JMMS');</script>
+		<!-- End Google Tag Manager -->
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col-md-8">
+					<h1>Trading Post Coming Soon</h1>
+					Sorry about that! This site is currently under construction. While we're working on making an awesome tool for all our Scouts and Scouters some things may be out of place. Don't hesitate to contact us with any questions you may have. We want to hear from you!
+				</div>
+				<div class="col-md-4">
+					<img class="img-fluid rounded" src="_img/coming_soon.jpg" alt="">
+				</div>
+			</div>
+		</div>
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
