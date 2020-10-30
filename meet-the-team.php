c6e78052a1a02cbadcb52f9d7d094d3f84269bcc
Matt Gutterman <mgutterman@unamilodge.org>
2020-10-30 15:34:36 -0400

    Changed image size

--- meet-the-team.php
+++ meet-the-team.php
@@ -19,10 +19,10 @@
 				</div>
 			</div>
 			<div class="row split">
-				<div class="hidden-sm-down col-md-2">
+				<div class="hidden-sm-down col-md-3">
 					<img class="img-fluid rounded" src="_img/profiles/steve.jpg" alt="">
 				</div>
-				<div class="col-12 col-md-10">
+				<div class="col-12 col-md-9">
 					<h2>Steve Mach <div class="h5">Camp Director</div></h2>
 					<p>
 					This will be Steve’s 13th summer on camp staff at Resica Falls. He has previously served as Handicraft Director, Commissioner, Program Director, and Assistant Camp Director, as well as the main architect of our 2020 Virtual Camp. He is an Eagle Scout from Troop 18, a Vigil Honor member of Unami Lodge, One, the Advisor of Venturing Crew 1957, a District Committee Member, and has completed Wood Badge training. Steve is a high school social studies teacher who is always looking for new ways to improve camp. He is excited for the opportunities 2021 will bring as he begins his first summer as Camp Director. Contact him at <a href="mailto:steve.mach@resicafalls.org">steve.mach@resicafalls.org</a>.
@@ -30,10 +30,10 @@
 				</div>
 			</div>
 			<div class="row split">
-				<div class="hidden-sm-down col-md-2">
+				<div class="hidden-sm-down col-md-3">
 					<img class="img-fluid rounded" src="_img/profiles/jerry.jpg" alt="">
 				</div>
-				<div class="col-12 col-md-10">
+				<div class="col-12 col-md-9">
 					<h2>Jerry Reed <div class="h5">Assistant Camp Director</div></h2>
 					<p>
 					Jerry has spent over 45 years on camp staff at five different camps. In the past, he has served as Quartermaster, Head Commissioner, COPE Director, Program Director, Business Manager, Evening Program Coordinator, and Health Officer, as well as Camp Director at three different camps – Camp Tri-Mount, Camp Barton, and Resica Falls. He has served on multiple National Camping School staffs throughout the region and has staffed several Wood Badge courses. A Vigil Honor member of Unami Lodge, One, Jerry oversees the logistical and administrative operations of camp, and serves as an Associate Lodge Adviser.  Contact him at <a href="mailto:jerry.reed@resicafalls.org">jerry.reed@resicafalls.org</a>.
@@ -41,10 +41,10 @@
 				</div>
 			</div>
 			<div class="row split">
-				<div class="hidden-sm-down col-md-2">
+				<div class="hidden-sm-down col-md-3">
 					<img class="img-fluid rounded" src="_img/profiles/jacob.jpg" alt="">
 				</div>
-				<div class="col-12 col-md-10">
+				<div class="col-12 col-md-9">
 					<h2>Jacob Huff <div class="h5">Program Director</div></h2>
 					<p>
 					An Eagle Scout, Jacob brings an abundance of enthusiasm to the position of Program Director. Jacob began his tenure on camp staff at Camp Karoondinha as a Counselor in Training in 1995. He served for the next six summers primarily in the Aquatics area of camp. He joined the Resica Falls family in 2010 as Shooting Sports Director and has served as the Head Commissioner since 2016. Jacob was vital to the success of our 2020 Virtual Camp where he served as the Assistant Head Counselor. Outside of camp, Jacob works in the field of special education and teaches music lessons. Contact him at <a href="mailto:jacob.huff@resicafalls.org">jacob.huff@resicafalls.org</a>.
@@ -52,10 +52,10 @@
 				</div>
 			</div>
 			<!-- <div class="row split">
-				<div class="hidden-sm-down col-md-2">
+				<div class="hidden-sm-down col-md-3">
 					<img class="img-fluid rounded" src="_img/profiles/andy.jpg" alt="">
 				</div>
-				<div class="col-12 col-md-10">
+				<div class="col-12 col-md-9">
 					<h2>Andy Connell <div class="h5">Activities Coordinator</div></h2>
 					<p>
 					Andy brings 9 years of camp staff experience to the table in his role of Activities Coordinator for Virtual Camp. This Eagle Scout has previously worked as Civics Director, Program Commissioner, and Assistant Program Director. Andy is an integral part of the success of Resica Falls. Andy’s favorite board game is Clue. Contact Andy at <a href="mailto:andy.connell@resicafalls.org">andy.connell@resicafalls.org</a>.
@@ -63,10 +63,10 @@
 				</div>
 			</div> -->
 			<div class="row split">
-				<div class="hidden-sm-down col-md-2">
+				<div class="hidden-sm-down col-md-3">
 					<img class="img-fluid rounded" src="_img/profiles/nick.jpg" alt="">
 				</div>
-				<div class="col-12 col-md-10">
+				<div class="col-12 col-md-9">
 					<h2>Nick McKee <div class="h5">Ranger</div></h2>
 					<p>
 					Nick is an Eagle Scout and Vigil Honor member of Unami Lodge, One who has served on camp staff since 2004. Starting as a Counselor in Training, he has worked in a variety of areas of camp, including Handicraft, Trading Post, Dining Hall, and Quartermaster. He also spent two years as the Assistant Ranger at Musser Scout Reservation. In 2015, Nick returned to Resica Falls as our Ranger, where his work ethic has been vital in continuing to improve our facilities and mark Resica as a destination camp throughout the Northeast Region. Contact Nick at <a href="mailto:nicholas.mckee@scouting.org">nicholas.mckee@scouting.org</a>.
@@ -74,10 +74,10 @@
 				</div>
 			</div>
 			<div class="row split">
-				<div class="hidden-sm-down col-md-2">
+				<div class="hidden-sm-down col-md-3">
 					<img class="img-fluid rounded" src="_img/profiles/chris.jpg" alt="">
 				</div>
-				<div class="col-12 col-md-10">
+				<div class="col-12 col-md-9">
 					<h2>Chris Brenner <div class="h5">Camp Director</div></h2>
 					<p>
 					Chris served as Camp Director at Resica Falls from 2006-2020. During his tenure, innovative programs were implemented, outstanding facilities were built, and camper attendance doubled. An Eagle Scout and Vigil Honor member of Unami Lodge, One, Chris served as Scoutmaster of Troop 226 in Philadelphia for 15 years, has sat on numerous district and council committees, and served as the Camp Director at Treasure Island in 1998. A retired elementary school teacher of 35 years, Chris continues to serve as a wise and valued mentor to the entire camp leadership team and will be spending lots of time in camp. Contact him at <a href="mailto:chris.brenner@resicafalls.org">chris.brenner@resicafalls.org</a>.
@@ -85,10 +85,10 @@
 				</div>
 			</div>
 			<div class="row split">
-				<div class="hidden-sm-down col-md-2">
+				<div class="hidden-sm-down col-md-3">
 					<img class="img-fluid rounded" src="_img/profiles/john.jpg" alt="">
 				</div>
-				<div class="col-12 col-md-10">
+				<div class="col-12 col-md-9">
 					<h2>John Bickel <div class="h5">Director of Outdoor Adventures</div></h2>
 					<p>
 					John is an Eagle Scout and Vigil Honor member of Unami Lodge, One who previously served as Dan Beard Director, Head Commissioner, and Program Director at Resica Falls. After two summers as the Camp Director at Musser Scout Reservation, this former lodge chief took a break from our camping operations, while continuing to serve on National Camping School staff. A professional scouter, John has served the Cradle of Liberty Council in a variety of positions, including district executive, council program director, and senior development director. In addition to Resica Falls and Musser, John oversees all things program in the council. Contact him at <a href="mailto:john.bickel@scouting.org">john.bickel@scouting.org</a>.
