289a1bcacc44d0cb19dfb4928958b17f4687b4c4
David Gibbons <dgibbns@gmail.com>
2021-01-21 06:14:30 -0500

    Created seek page

---
+++ seek.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include '_inc/html_head.php'; ?>
+		<meta name="description" content="Spend your summer doing something amazing at Resica Falls Scout Reservation." />
+		<title>SEEK <?php include '_inc/var/site_name.php'; ?></title>
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5R8JMMS');</script>
+		<!-- End Google Tag Manager -->
+	</head>
+	<body>
+		<?php include '_inc/header.php'; ?>
+		<?php include '_inc/nav.php'; ?>
+
+		<div class="container content">
+			<div class="row">
+				<div class="col">
+					<h1>SEEK - Staff Education, Experiences, & Knowledge</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8">
+					
+				<p>New for 2021, the SEEK program is focused on preparing the next generation of camp staff. SEEK replaces our traditional CIT program with a three week program designed around targeted skill development and personal growth.  Scouts between the ages of 14 and 16 will work with our dedicated staff to hone the skills needed to be a great leader, both in camp and in their units.</p>  
+
+				<p>SEEK was designed with 4 goals in mind:</p>
+
+				<ol>
+					<li>Develop and foster the core traits of strong leadership and character.</li>
+					<li>Identify areas of personal growth in and outside of Scouting.</li>
+					<li>Provide hands-on opportunities to practice skill instruction in a summer camp environment.</li>
+					<li>Provide a fun and meaningful program for more experienced Scouts looking for their next adventure.</li>
+				</ol>
+
+				<p>To accomplish this, Resica Falls is taking all the steps necessary to make this a top-notch program! The SEEK program will have its own staff, live in it’s own campsite, and will have its own program specific uniforms, trips, and activities.</p>
+
+				<p>Each of the 3 weeks has both trainings and activities centered around a theme:</p>
+
+				<ul>
+					<li>Week 1 - Connecting for Growth</li>
+					<li>Week 2 - Leading and Learning</li>
+					<li>Week 3 - Action and Adventure</li>
+				</ul>
+
+				<p>Care has been taken to provide a balance of both work and play over this extended period. One day SEEK participants might be shadowing an area director and the next day camping out Lake Roger!</p>
+
+				<p>We’re thrilled to be offering the SEEK program in 2021! Space is limited, so register today to ensure your spot in this exciting new adventure!</p>
+
+				<p>Registration Coming Soon</p>
+
+				</div>
+				<div class="col-md-4">
+					<img class="img-fluid rounded" src="_img/staff.jpg" alt="">
+				</div>
+			</div>
+		</div>
+
+		<?php include '_inc/footer.php'; ?>	
+		<?php include '_inc/html_foot.php'; ?>
+	</body>
+</html>
