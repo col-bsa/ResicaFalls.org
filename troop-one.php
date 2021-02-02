597e63a73d6e2276a73931987d269b13e9937c15
David Gibbons <dgibbns@gmail.com>
2021-02-01 20:52:28 -0500

    Added registration links to troop one page

--- troop-one.php
+++ troop-one.php
@@ -34,9 +34,32 @@
 					<p>
 						Troop One is no different than any other week in camp except that if you are spending more than 1 week in camp, it is only half the normal price which can be found on our <a href="dates-and-rates">Dates and Rates</a> page. This deal includes stays at other camps.
 					</p>
-					<p>
-						Please visit our <a href="dates-and-rates">Dates and Rates</a> page for more information about pricing and to sign up!
-					</p>
+					<table class="table text-center table-sessions">
+						<thead class="thead-inverse">
+							<tr>
+								<th class="text-center">Session</th>
+								<th class="text-center">Dates</th>
+								<th class="text-center">Registration</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Week 1-5</td>
+								<td>June 27 - July 31</td>
+								<td>Closed (Full)</td>
+							</tr>
+							<tr>
+								<td>Week 6</td>
+								<td>August 1 - August 7</td>
+								<td><a class="btn btn-success" href="https://colbsa.doubleknot.com/event/scouts-bsaventuring-summer-camp-at-resica-falls-week-6/2693062">Register</a></td>
+							</tr>
+							<tr>
+								<td>Week 7</td>
+								<td>August 8 - August 14</td>
+								<td><a class="btn btn-success" href="https://colbsa.doubleknot.com/event/scouts-bsaventuring-summer-camp-at-resica-falls-week-7/2693063">Register</a></td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
 			<div class="row">
